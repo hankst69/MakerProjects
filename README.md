ebc325266ab6e9ad80cc708beaedd1cbd5e9d3ae
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-02-15 13:39:02 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MakerProjects
 Summary repository for several Maker projects and related KnowHow
+
+### Find related infos in 'KnowHow' for
+* Boards and Chips (Schematics, Pins etc.)
+* suggested Arduino Libraries (for boards of interest)
+* infos about ptotocols (I2C, RS485, ...)
+
+### Clone working projects into their intended sub directories via:
+* clone_espBode
+* clone fygen
+* clone html_rtsp_plater
+
---
+++ clone_in_folder.bat
@@ -0,0 +1,51 @@
+@echo off
+rem %1: target-folder
+rem %2: git-repo-url
+if "%~1" equ "" echo missing argument 'target-folder' &goto :Usage
+if "%~2" equ "" echo missing argument 'git-repo-url' &goto :Usage
+goto :Start
+:Usage
+echo.
+echo USAGE: %~n0 target-folder git-repo-url
+echo.
+goto :EOF
+
+:Start
+grep "%~2" "%~dp1.git\config" 1>NUL 2>NUL
+rem echo %ERRORLEVEL%
+if %ERRORLEVEL% equ 0 (
+  echo '%~2' is already cloned into '%~dp1'
+  echo.
+  if exist "%~dp1clone.bat"     echo delete the content in folder '%~dp1' ^(except file clone.bat^) to clone fresh
+  if not exist "%~dp1clone.bat" echo delete the content in folder '%~dp1' to clone fresh
+  goto :EOF
+)
+if %ERRORLEVEL% equ 1 (
+  echo a different repository than '%~2' is already cloned into '%~dp1'
+  echo.
+  echo currently cloned: 
+  grep ".git" .git\config
+  echo.
+  if exist "%~dp1clone.bat"     echo delete the content in folder '%~dp1' ^(except file clone.bat^) to clone fresh
+  if not exist "%~dp1clone.bat" echo delete the content in folder '%~dp1' to clone fresh
+  goto :EOF
+)
+
+:Clone
+if not exist "%~dp1" mkdir "%~dp1"
+echo cloning "%~2" into "%~dp1"
+if exist "%~dp1clone.bat" (
+  if exist "%~dp0clone.tmp" (del /F /Q "%~dp0clone.tmp")
+  copy "%~dp1clone.bat" "%~dp0clone.tmp"
+  del /F /Q "%~dp1clone.bat"
+)
+
+rem echo %cd%
+pushd "%~dp1"
+git clone "%~2" .
+popd
+
+if exist "%~dp0clone.tmp" (
+  copy "%~dp0clone.tmp" "%~dp1clone.bat"
+  del /F /Q "%~dp0clone.tmp"
+)
