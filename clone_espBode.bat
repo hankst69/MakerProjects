4bf24a422a41cf1c2cbb69c0e540e94fbbcf99b4
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-02-15 13:39:02 +0100

    initial commit

---
+++ clone_espBode.bat
@@ -0,0 +1,5 @@
+@rem https://github.com/hankst69/espBode.git
+@rem https://github.com/Hamhackin/espBode
+@rem https://github.com/timkoers/espBode
+@rem https://github.com/sq6sfo/espBode
+@call "%~dp0clone_in_folder.bat" "%~dp0espBode\" "https://github.com/hankst69/espBode.git"
---
+++ clone_fygen.bat
@@ -0,0 +1,3 @@
+@rem "https://github.com/hankst69/fygen.git"
+@rem "https://github.com/mattwach/fygen"
+@call "%~dp0clone_in_folder.bat" "%~dp0fygen\" "https://github.com/hankst69/fygen.git"
