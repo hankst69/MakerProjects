8a4f0503aab61a4c92d1f7162c9331fe4e938852
hankst69 <steffen_hanke@web.de>
2024-02-17 13:49:48 +0100

    added clone_solar.bat - updated clone scripts

--- clone_espBode.bat
+++ clone_espBode.bat
@@ -3,3 +3,6 @@
 @rem https://github.com/timkoers/espBode
 @rem https://github.com/sq6sfo/espBode
 @call "%~dp0clone_in_folder.bat" "%~dp0espBode\" "https://github.com/hankst69/espBode.git"
+@cd "%~dp0espBode"
+@git switch awgDevices
+@git status
--- clone_html5_rtsp_player.bat
+++ clone_html5_rtsp_player.bat
@@ -1,3 +1,5 @@
 @rem https://github.com/hankst69/html5_rtsp_player
 @rem https://github.com/Streamedian/html5_rtsp_player
 @call "%~dp0clone_in_folder.bat" "%~dp0html5_rtsp_player\" "https://github.com/hankst69/html5_rtsp_player"
+@cd "%~dp0html5_rtsp_player"
+@git status
