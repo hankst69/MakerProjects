4dff8a24569e55f6b4220fc833b139bb4971daba
hankst69 <steffen_hanke@web.de>
2024-02-17 13:49:48 +0100

    added clone_solar.bat - updated clone scripts

--- clone_fygen.bat
+++ clone_fygen.bat
@@ -1,3 +1,5 @@
 @rem "https://github.com/hankst69/fygen.git"
 @rem "https://github.com/mattwach/fygen"
 @call "%~dp0clone_in_folder.bat" "%~dp0fygen\" "https://github.com/hankst69/fygen.git"
+@cd "%~dp0fygen"
+@git status
---
+++ clone_solar.bat
@@ -0,0 +1,2 @@
+@call "%~dp0clone_in_folder.bat" "%~dp0Solar2024\" "https://github.com/hankst69/Solardach_2024.git"
+@cd "%~dp0Solar2024"
