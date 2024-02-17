4239d4c39e462069c0b396f026c621bcd16bfbe0
hankst69 <steffen_hanke@web.de>
2024-02-17 19:30:24 +0100

    deleted the "local" clone.bat scripts since the concept is to cumbersome and the risk to commit a clone.bat unwillingly into a target repository is high

--- espBode/clone.bat
+++
@@ -1,5 +0,0 @@
-@rem https://github.com/hankst69/espBode.git
-@rem https://github.com/Hamhackin/espBode
-@rem https://github.com/timkoers/espBode
-@rem https://github.com/sq6sfo/espBode
-@call "%~dp0..\clone_in_folder.bat" "%0" "https://github.com/hankst69/espBode.git"
--- fygen/clone.bat
+++
@@ -1,3 +0,0 @@
-@rem "https://github.com/mattwach/fygen"
-@rem "https://github.com/hankst69/fygen.git"
-@call "%~dp0..\clone_in_folder.bat" "%0" "https://github.com/hankst69/fygen.git"
