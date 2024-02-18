e6f1f78bc0a8043a078411dc78c4b9cd5e18333d
hankst69 <steffen_hanke@web.de>
2024-02-18 22:29:38 +0100

    update clone scripts and .gitignore

--- clone_espBode.bat
+++ clone_espBode.bat
@@ -1,8 +1,7 @@
-@rem https://github.com/hankst69/espBode.git
+@rem https://github.com/hankst69/espBode
 @rem https://github.com/Hamhackin/espBode
 @rem https://github.com/timkoers/espBode
 @rem https://github.com/sq6sfo/espBode
-@call "%~dp0clone_in_folder.bat" "%~dp0espBode\" "https://github.com/hankst69/espBode.git"
-@cd "%~dp0espBode"
-@git switch awgDevices
-@git status
+rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0espBode\" "https://github.com/hankst69/espBode.git" --changeDir
+rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0ArduinoSketches\esp01\espBode\" "https://github.com/hankst69/espBode.git" --changeDir
+@call "%~dp0scripts\clone_in_folder.bat" "%~dp0ProjectAWGControl\espBode\" "https://github.com/hankst69/espBode.git" --changeDir --switchBranch awgDevices
