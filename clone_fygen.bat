06e82131a2dbe30a04d5f1ac0b4d556bc6d9e6d1
hankst69 <steffen_hanke@web.de>
2024-02-18 23:52:08 +0100

    another update to clone scripts

--- clone_fygen.bat
+++ clone_fygen.bat
@@ -1,5 +1,3 @@
 @rem "https://github.com/hankst69/fygen"
 @rem "https://github.com/mattwach/fygen"
 @call "%~dp0scripts\clone_in_folder.bat" "%~dp0ProjectAWGControl\fygen\" "https://github.com/hankst69/fygen.git" --changeDir
-@git remote -v
-@git status
