fc006c183895910de4880c9390dd13ce370e21e6
hankst69 <steffen_hanke@web.de>
2024-02-18 23:52:08 +0100

    another update to clone scripts

--- clone_espBode.bat
+++ clone_espBode.bat
@@ -2,6 +2,6 @@
 @rem https://github.com/Hamhackin/espBode
 @rem https://github.com/timkoers/espBode
 @rem https://github.com/sq6sfo/espBode
-rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0espBode\" "https://github.com/hankst69/espBode.git" --changeDir
-rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0ArduinoSketches\esp01\espBode\" "https://github.com/hankst69/espBode.git" --changeDir
+@rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0espBode\" "https://github.com/hankst69/espBode.git" --changeDir
+@rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0ArduinoSketches\esp01\espBode\" "https://github.com/hankst69/espBode.git" --changeDir
 @call "%~dp0scripts\clone_in_folder.bat" "%~dp0ProjectAWGControl\espBode\" "https://github.com/hankst69/espBode.git" --changeDir --switchBranch awgDevices
--- scripts/clone_in_folder.bat
+++ scripts/clone_in_folder.bat
@@ -1,8 +1,8 @@
 @echo off
 rem %1: target-folder
 rem %2: git-repo-url
-if "%~1" equ "" echo missing argument 'target-folder' &goto :Usage
-if "%~2" equ "" echo missing argument 'git-repo-url' &goto :Usage
+if "%~1" equ "" echo error: missing argument 'target-folder' &goto :Usage
+if "%~2" equ "" echo error: missing argument 'git-repo-url' &goto :Usage
 goto :Start
 :Usage
 echo.
@@ -14,50 +14,72 @@ goto :EOF
 grep "%~2" "%~dp1.git\config" 1>NUL 2>NUL
 rem echo %ERRORLEVEL%
 if %ERRORLEVEL% equ 0 (
-  echo '%~2' is already cloned into '%~dp1'
+  echo ********************************************************************************
+  echo * '%~2' is already cloned into '%~dp1'
+  echo ********************************************************************************
   echo.
-  if exist "%~dp1clone.bat"     echo delete the content in folder '%~dp1' ^(except file clone.bat^) to clone fresh
-  if not exist "%~dp1clone.bat" echo delete the content in folder '%~dp1' and try again to clone fresh
-  if not exist "%~dp1clone.bat" echo use 'rmdir /s /q "%~dp1"' to rmove the folder
+  echo ********************************************************************************
+  echo * the status in '%~dp1' is:
+  echo ********************************************************************************
+  pushd "%~dp1"
+  git remote -v
+  if /I "%~4" equ "--switchBranch" (echo. & git switch %~5)
+  git status
+  popd
+  if /I "%~3" equ "--changeDir" (cd "%~dp1")
+  echo.
+  echo ********************************************************************************
+  if exist "%~dp1clone.bat"     echo * to delete the content in folder '%~dp1' ^(except file clone.bat^) to clone fresh
+  if not exist "%~dp1clone.bat" echo * to delete the content in folder '%~dp1' and try again to clone fresh
+  if not exist "%~dp1clone.bat" echo * use 'rmdir /s /q "%~dp1"' to rmove the folder
+  echo ********************************************************************************
   echo.
   goto :EOF
 )
 if %ERRORLEVEL% equ 1 (
-  echo a different repository than '%~2' is already cloned into '%~dp1'
+  echo ********************************************************************************
+  echo * a different repository than '%~2' is already cloned into '%~dp1'
+  echo ********************************************************************************
   echo.
-  echo currently cloned: 
+  echo ********************************************************************************
+  echo * currently cloned: 
+  echo ********************************************************************************
   grep ".git" .git\config
   echo.
-  if exist "%~dp1clone.bat"     echo delete the content in folder '%~dp1' ^(except file clone.bat^) to clone fresh
-  if not exist "%~dp1clone.bat" echo delete the content in folder '%~dp1' and try again to clone fresh
-  if not exist "%~dp1clone.bat" echo use 'rmdir /s /q "%~dp1"' to remove the folder
+  echo ********************************************************************************
+  if exist "%~dp1clone.bat"     echo * to delete the content in folder '%~dp1' ^(except file clone.bat^) to clone fresh
+  if not exist "%~dp1clone.bat" echo * to delete the content in folder '%~dp1' and try again to clone fresh
+  if not exist "%~dp1clone.bat" echo * use 'rmdir /s /q "%~dp1"' to remove the folder
+  echo ********************************************************************************
   echo.
   goto :EOF
 )
 
 :Clone
+echo ********************************************************************************
+echo * cloning "%~2" into "%~dp1"
+echo ********************************************************************************
 if not exist "%~dp1" mkdir "%~dp1"
-echo cloning "%~2" into "%~dp1"
 if exist "%~dp1clone.bat" (
   if exist "%~dp0clone.tmp" (del /F /Q "%~dp0clone.tmp")
   copy "%~dp1clone.bat" "%~dp0clone.tmp"
   del /F /Q "%~dp1clone.bat"
 )
-
-rem cd
-rem git clone "%~2" "%~dp1"
 pushd "%~dp1"
 cd
 git clone "%~2" .
-popd
-
+set _CLONE_ERROR=%ERRORLEVEL%
 if exist "%~dp0clone.tmp" (
   copy "%~dp0clone.tmp" "%~dp1clone.bat"
   del /F /Q "%~dp0clone.tmp"
 )
-
+if %_CLONE_ERROR% neq 0 (echo. & echo error: git clone failed & popd & goto:EOF)
 echo.
-@git remote -v
-@git status
+echo ********************************************************************************
+echo * the status in '%~dp1' is:
+echo ********************************************************************************
+git remote -v
+if /I "%~4" equ "--switchBranch" (echo. & git switch %~5)
+git status
+popd
 if /I "%~3" equ "--changeDir" (cd "%~dp1")
-if /I "%~4" equ "--switchBranch" (echo.& git switch %~5 & git status)
