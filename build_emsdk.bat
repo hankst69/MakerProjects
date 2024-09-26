2a752858c27177cada7ac9420b8042e645228d34
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-09-26 14:25:01 +0200

    suport EMSDK building and fix a bug in basic clone script

---
+++ build_emsdk.bat
@@ -0,0 +1,37 @@
+@echo off
+set "_MAKER_ROOT=%~dp0"
+rem https://emscripten.org/docs/getting_started/downloads.html
+
+set "_EMSDK_DIR=%_MAKER_ROOT%Emsdk"
+if not exist "%_EMSDK_DIR%" mkdir "%_EMSDK_DIR%"
+
+rem set _EMSDK_VERSION=1.38.45
+rem set _EMSDK_VERSION=3.1.67
+set _EMSDK_VERSION=latest
+if "%~1" neq "" set "_EMSDK_VERSION=%~1"
+
+set "_EMSDK_BIN_DIR=%_EMSDK_DIR%\%_EMSDK_VERSION%"
+if not exist "%_EMSDK_BIN_DIR%" mkdir "%_EMSDK_BIN_DIR%"
+
+pushd "%_MAKER_ROOT%"
+call "%_MAKER_ROOT%\scripts\clone_in_folder.bat" "%_EMSDK_BIN_DIR%" "https://github.com/emscripten-core/emsdk.git"
+popd
+
+rem test python
+call which python.exe 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 goto :test_python_success
+echo error: python is not available
+goto :EOF
+:test_python_success
+
+
+pushd "%_EMSDK_BIN_DIR%"
+rem # Fetch the latest version of the emsdk (not needed the first time you clone)
+call git pull
+rem # Download and install the latest SDK tools.
+call emsdk.bat install %_EMSDK_VERSION%
+# Make the "latest" SDK "active" for the current user. (writes .emscripten file)
+call emsdk.bat activate %_EMSDK_VERSION%
+popd
+
+cd "%_EMSDK_BIN_DIR%"
--- build_qt.bat
+++ build_qt.bat
@@ -1,17 +1,18 @@
 @echo off
+set "_MAKER_ROOT=%~dp0"
 rem https://doc.qt.io/qt-6/getting-sources-from-git.html
 rem https://doc.qt.io/qt-6/configure-options.html
 rem https://doc.qt.io/qt-6/build-sources.html
 rem https://doc.qt.io/qt-6/windows-building.html
 rem https://code.qt.io/cgit
 
-set "_QT_VERSION=6.6.3"
+set _QT_VERSION=6.6.3
 if "%~1" neq "" set "_QT_VERSION=%~1"
 
 rem 1) cloning QT sources
 rem defines: _QT_DIR
 rem defines: _QT_SOURCES_DIR
-call "%~dp0.\clone_qt.bat" %_QT_VERSION%
+call "%_MAKER_ROOT%\clone_qt.bat" %_QT_VERSION%
 if "%_QT_DIR%" EQU "" (echo cloning Qt %_QT_VERSION% failed &goto :EOF)
 if "%_QT_SOURCES_DIR%" EQU "" (echo cloning Qt %_QT_VERSION% failed &goto :EOF)
 if not exist "%_QT_DIR%" (echo cloning Qt %_QT_VERSION% failed &goto :EOF)
@@ -21,6 +22,7 @@ set "_QT_BUILD_DIR=%_QT_DIR%\qt_build_%_QT_VERSION%"
 set "_QT_BIN_DIR=%_QT_DIR%\qt%_QT_VERSION%"
 
 rem test msvs
+rem ...tbd
 :test_msvs_success
 rem test cmake
 call which cmake.exe 1>nul 2>nul
@@ -29,8 +31,10 @@ echo error: cmake is not available
 goto :EOF
 :test_cmake_success
 rem test ninja
+rem ...tbd
 :test_ninja_success
 rem test llvm (set LLVM_INSTALL_DIR + need to set the FEATURE_clang and FEATURE_clangcpp CMake variable to ON to re-evaluate this checks)
+rem ...tbd
 :test_llvm_success
 rem test perl (+ gperf + qnx)
 call which perl.exe 1>nul 2>nul
@@ -73,10 +77,11 @@ if not exist "%_QT_BIN_DIR%\bin\Qt6WebSockets.dll" (
   pushd "%_QT_BUILD_DIR%"
   call cmake --install .
   popd
-  if not exist "%_QT_BIN_DIR%\bin\Qt6WebSockets.dll" echo error: QT-INSTALL FAILED&goto :EOF
+  if not exist "%_QT_BIN_DIR%\bin\Qt6WebSockets.dll" echo error: QT-INSTALL FAILED&goto :qt_install_done
 )
+echo QT-INSTALL done
 call which Qt6WebSockets.dll 1>nul 2>nul
-if %ERRORLEVEL% EQU 0 echo QT-INSTALL done&goto :qt_install_done
+if %ERRORLEVEL% EQU 0 goto :qt_install_done
 set "PATH=%PATH%;%_QT_BIN_DIR%\bin"
 :qt_install_done
 
