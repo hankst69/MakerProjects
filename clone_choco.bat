5c87cc116dfac36a413875559ac4ea0aa153080b
hankst69 <steffen_hanke@web.de>
2024-10-02 23:37:48 +0200

    updated clone_choco

--- clone_choco.bat
+++ clone_choco.bat
@@ -1,12 +1,7 @@
 @echo off
-set "_MAKER_ROOT=%~dp0"
 rem https://de.wikipedia.org/wiki/Chocolatey
 rem https://chocolatey.org/
 rem https://github.com/chocolatey/choco.git
-
-set "_CHOCO_DIR=%~dp0Choco"
-if not exist "%_CHOCO_DIR%" mkdir "%_CHOCO_DIR%"
-
-set "_CHOCO_SOURCES_DIR=%_CHOCO_DIR%\Sources\"
-
+set "_CHOCO_DIR=%~dp0CSharp\Choco"
+set "_CHOCO_SOURCES_DIR=%_CHOCO_DIR%"
 call "%_MAKER_ROOT%\scripts\clone_in_folder.bat" "%_CHOCO_SOURCES_DIR%" "https://github.com/chocolatey/choco.git" --changeDir
