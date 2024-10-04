819e2a7a1cc8b3bb2b79f008fc8a16294abde8e5
hankst69 <steffen_hanke@web.de>
2024-10-05 00:18:40 +0200

    fixed build_choco build_vesc and build_qt-creator

--- build_qt-creator.bat
+++ build_qt-creator.bat
@@ -1,12 +1,15 @@
 @echo off
 set "_MAKER_ROOT=%~dp0"
 rem https://github.com/vedderb/bldc?tab=readme-ov-file#on-all-platforms
+rem https://pypi.org/project/aqtinstall/#:~:text=Same%20as%20usual%2C%20it%20can%20be%20installed%20with,some%20of%20which%20are%20precompiled%20in%20several%20platforms.
+
+set _QT_VERSION=6.6.3
+if "%~1" neq "" set "_QT_VERSION=%~1"
 
 set "_QT_DIR=%~dp0Qt"
 if not exist "%_QT_DIR%" mkdir "%_QT_DIR%"
 
 set "_QT_CREATOR_ENV_DIR=%_QT_DIR%\.qt_creator"
-
 
 rem echo test python
 call which python.exe 1>nul 2>nul
@@ -38,16 +41,33 @@ set _VERSION_NR=
 
 
 rem -- install qt-creator in a dedicated python environment
+call deactivate 1>nul 2>nul
 if not exist "%_QT_CREATOR_ENV_DIR%\.venv.created" (
   echo creating Qt-Creator environment ... ^(%_QT_CREATOR_ENV_DIR%^)
   if not exist "%_QT_CREATOR_ENV_DIR%" mkdir "%_QT_CREATOR_ENV_DIR%"
   call python -m venv "%_QT_CREATOR_ENV_DIR%" || exit /b
   call "%_QT_CREATOR_ENV_DIR%\Scripts\activate.bat"
+  rem
   echo installing Qt-Creator ...
   call python -m pip install --upgrade pip  || exit /b
   call python -m pip install aqtinstall  || exit /b
+  rem
+  rem call python -m aqt list-qt windows desktop --arch %_QT_VERSION%
+  echo.
+  echo aqt list-qt windows desktop --arch %_QT_VERSION%
+  call aqt list-qt windows desktop --arch %_QT_VERSION%
+  echo.
+  echo aqt list-qt windows desktop --modules %_QT_VERSION% wasm_multithread
+  call aqt list-qt windows desktop --modules %_QT_VERSION% wasm_multithread
+  rem call aqt list-qt windows desktop --modules %_QT_VERSION% wasm_singlethread
+  echo.
+  echo aqt list-qt windows desktop --modules %_QT_VERSION% win64_mingw
+  call aqt list-qt windows desktop --modules %_QT_VERSION% win64_mingw
+  echo.
+  rem call aqt install-qt windows desktop %_QT_VERSION% win64_mingw -m all
+  pushd 
   call make qt_install || exit /b
-  rem python -m pip install setuptools^>=36.0.0,^<58.0.0 wheel --no-cache-dir || exit /b
+  rem python -m pip install setuptools^>=36.0.0,^<58.0.0 wheel --no-cache-dir ||call deactivate& exit /b
   echo done >"%_QT_CREATOR_ENV_DIR%\.venv.created"
 )
 echo Qt-Creator ready ... ^(%_QT_CREATOR_ENV_DIR%^)
--- build_vesc-tool.bat
+++ build_vesc-tool.bat
@@ -5,6 +5,13 @@ rem https://vesc-project.com/node/309
 rem https://github.com/vedderb/vesc_tool
 rem http://github.com/vedderb/bldc
 
+set "_QT_DIR=%~dp0Qt"
+if not exist "%_QT_DIR%" mkdir "%_QT_DIR%"
+set "_QT_ENV_DIR=%_QT_DIR%\.qt_env"
+
+set "_TOOLS_DIR=%_MAKER_ROOT%\.tools"
+set "_TOOLS_QTCREATOR_DIR=%_TOOLS_DIR%\.qtcreator"
+
 
 echo.
 echo 1) clone VESC FW and VESC Tool
@@ -12,47 +19,141 @@ call "%_MAKER_ROOT%\clone_vesc.bat"
 rem defines: _VESC_DIR
 rem defines: _VESC_TOOL_DIR
 rem defines: _VESC_FW_DIR
-if "%_VESC_DIR%" EQU "" (echo cloning VESC failed &goto :EOF)
-if "%_VESC_TOOL_DIR%" EQU "" (echo cloning VESC-Tool failed &goto :EOF)
-if "%_VESC_FW_DIR%" EQU "" (echo cloning VESC-FW failed &goto :EOF)
-if not exist "%_VESC_DIR%" (echo cloning VESC failed &goto :EOF)
-if not exist "%_VESC_TOOL_DIR%" (echo cloning VESC-Tool failed &goto :EOF)
-if not exist "%_VESC_FW_DIR%" (echo cloning VESC-FW failed &goto :EOF)
+if "%_VESC_DIR%" EQU "" (echo error: cloning VESC failed &goto :EOF)
+if "%_VESC_TOOL_DIR%" EQU "" (echo error: cloning VESC-Tool failed &goto :EOF)
+if "%_VESC_FW_DIR%" EQU "" (echo error: cloning VESC-FW failed &goto :EOF)
+if not exist "%_VESC_DIR%" (echo error: cloning VESC failed &goto :EOF)
+if not exist "%_VESC_TOOL_DIR%" (echo error: cloning VESC-Tool failed &goto :EOF)
+if not exist "%_VESC_FW_DIR%" (echo error: cloning VESC-FW failed &goto :EOF)
+
+
+rem pushd "%_VESC_FW_DIR%"
+rem call make | grep "supported boards"
+rem popd
+set _VESC_BOARD=100_250
+
 
 echo.
-echo 2) install Qt
-pushd %_MAKER_ROOT%
-call "%_MAKER_ROOT%\build_qt.bat" 6.6.3
-echo.
-popd
-if not exist "%_QT_BIN_DIR%\bin\Qt6WebSockets.dll" (
-  echo QT is not installed
-  goto :EOF
-)
-
-rem echo test make
+echo 1) install Make
 call which make 1>nul 2>nul
 if %ERRORLEVEL% EQU 0 goto :test_make_success
-call which nmake 1>nul 2>nul
-if %ERRORLEVEL% NEQ 0 goto :test_make_failed
-doskey make=nmake
-rem call which make 1>nul 2>nul
-rem if %ERRORLEVEL% EQU 0 goto :test_make_success
-goto :test_make_success
+call "%_MAKER_ROOT%\build_choco.bat"
+call choco install make
+call which make 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 goto :test_make_success
 :test_make_failed
 echo error: MAKE is not available
 goto :EOF
 :test_make_success
-echo  make available
+set _VERSION_NR=
+for /f "tokens=1,2,* delims= " %%i in ('call make --version') do if /I "%%j" equ "make" set "_VERSION_NR=%%k"
+rem for /f "tokens=6,* delims= " %%i in ('"%_QT_CREATOR_ENV_DIR%\Scripts\make.bat" --version') do set "_VERSION_NR=%%j"
+echo using: make %_VERSION_NR%
+set _VERSION_NR=
 
 echo.
-echo 3) configure VESC-Tool build
-echo ...tbd
+echo.
+echo 2) build VESC-FW SDK
+pushd "%_VESC_FW_DIR%"
+call make arm_sdk_install
+popd
 
 echo.
-echo 4) build VESC-Tool
-echo ...tbd
+echo.
+echo 3) build VESC-Board FW
+pushd "%_VESC_FW_DIR%"
+call make | grep "supported boards"
+rem
+call make %_VESC_BOARD%
+popd
+dir "%_VESC_FW_DIR%\builds\%_VESC_BOARD%"
+popd
 
 echo.
-echo 5) install VESC-Tool
+echo.
+echo 4) install Qt
+call which qtcreator.bat 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+  echo QtCreator already available
+  goto :test_qtcreator_success
+)
+if exist "%_TOOLS_DIR%\qtcreator.bat" set "Path=%Path%;%_TOOLS_DIR%;%_TOOLS_QTCREATOR_DIR%\bin"
+call which qtcreator.bat 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+  echo QtCreator already available
+  goto :test_qtcreator_success
+)
+rem pushd %_MAKER_ROOT%
+rem call "%_MAKER_ROOT%\build_qt.bat" 6.6.3
+rem echo.
+rem popd
+rem if not exist "%_QT_BIN_DIR%\bin\Qt6WebSockets.dll" (
+rem   echo QT is not installed
+rem   goto :EOF
+rem )
+
+rem echo test python
+call which python.exe 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 goto :test_python_success
+echo error: python not available
+goto :EOF
+:test_python_success
+set _VERSION_NR=
+for /f "tokens=1,2 delims= " %%i in ('call python --version') do set "_VERSION_NR=%%j"
+echo using: python %_VERSION_NR%
+set _VERSION_NR=
+
+rem -- install qt-creator via a aqt tool from a dedicated python environment
+call deactivate 1>nul 2>nul
+if not exist "%_VESC_FW_DIR%\tools\Qt\Tools\QtCreator\bin\qtcreator.exe" (
+  echo creating Qt environment ... ^(%_QT_ENV_DIR%^)
+  if not exist "%_QT_ENV_DIR%" mkdir "%_QT_ENV_DIR%"
+  call python -m venv "%_QT_ENV_DIR%" || exit /b
+  call "%_QT_ENV_DIR%\Scripts\activate.bat"
+  rem
+  echo installing AQT ...
+  call python -m pip install --upgrade pip  || exit /b
+  call python -m pip install aqtinstall  || exit /b
+  rem
+  echo.
+  pushd "%_VESC_FW_DIR%"
+  call make qt_install || exit /b
+  popd
+  echo done >"%_QT_ENV_DIR%\.venv.created"
+  call deacrtivate
+)
+echo Qt ready ... ^(%_QT_ENV_DIR%^)
+
+if not exist "%_VESC_FW_DIR%\tools\Qt\Tools\QtCreator\bin\qtcreator.exe" (
+  echo. error: building QtCreator failed
+  goto :EOF
+)
+if not exist "%_TOOLS_QTCREATOR_DIR%" mkdir "%_TOOLS_QTCREATOR_DIR%"
+call xcopy /S /Y /Q "%_VESC_FW_DIR%\tools\Qt\Tools\QtCreator" "%_TOOLS_QTCREATOR_DIR%" 1>NUL
+
+echo @pushd "%_TOOLS_QTCREATOR_DIR%\bin">"%_TOOLS_DIR%\qtcreator.bat"
+echo @call qtcreator.exe %%* >>"%_TOOLS_DIR%\qtcreator.bat"
+echo @popd>>"%_TOOLS_DIR%\qtcreator.bat"
+
+call which qtcreator.bat 1>nul 2>nul
+if %ERRORLEVEL% NEQ 0 set "Path=%Path%;%_TOOLS_DIR%;%_TOOLS_QTCREATOR_DIR%\bin"
+call which qtcreator.bat 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 goto :test_qtcreator_success
+  
+call "%_VESC_FW_DIR%\tools\Qt\Tools\QtCreator\bin\qtcreator.exe"
+:test_qtcreator_failed
+echo. error: QtCreator not available
+goto :EOF
+
+:test_qtcreator_success
+echo.
+call which qtcreator -l -d
+call qtcreator
+goto :EOF
+
+
+
+echo.
+echo.
+echo 6) install VESC-Tool
 echo ...tbd
