ffa6259aee03bd341b8d220290152f753b2d9064
hankst69 <steffen_hanke@web.de>
2024-10-04 20:08:51 +0200

    updated clone_choco and build_choco

--- build_choco.bat
+++ build_choco.bat
@@ -6,19 +6,32 @@ echo.
 echo 1) clone CHOCO
 call "%_MAKER_ROOT%\clone_choco.bat"
 rem defines: _CHOCO_DIR
-rem defines: _CHOCO_SOURCES_DIR
 if "%_CHOCO_DIR%" EQU "" (echo cloning CHOCO &goto :EOF)
-if "%_CHOCO_SOURCES_DIR%" EQU "" (echo cloning CHOCO &goto :EOF)
 if not exist "%_CHOCO_DIR%" (echo cloning CHOCO &goto :EOF)
-if not exist "%_CHOCO_SOURCES_DIR%" (echo cloning CHOCO &goto :EOF)
+
 
 echo.
 echo 2) build CHOCO
-pushd %_CHOCO_SOURCES_DIR%
-call "%_CHOCO_SOURCES_DIR%\build.bat"
+call which choco.exe 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 goto :test_choco_success
+if not exist "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\choco.exe" (
+  pushd %_CHOCO_DIR%
+  call "%_CHOCO_DIR%\build.bat"
+  popd
+)
+if not exist "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\choco.exe" (
+  echo. error: building Choco failed
+  goto :EOF
+)
+if not exist "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\lib" mkdir "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\lib"
+set "Path=%Path%;%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged"
+call which choco.exe 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 goto :test_choco_success
+echo. error: Choco build seems not to work
+goto :EOF
+:test_choco_success
 echo.
-popd
-rem if not exist "%_QT_BIN_DIR%\bin\Qt6WebSockets.dll" (
-rem   echo QT is not installed
-rem   goto :EOF
-rem )
+set _VERSION_NR=
+for /f "tokens=1,2,* delims=-" %%i in ('call choco --version') do set "_VERSION_NR=%%i-%%j-%%k"
+echo choco %_VERSION_NR%
+set _VERSION_NR=
