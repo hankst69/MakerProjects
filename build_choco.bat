93320dbada2f37859a442c172d3b93684db85e09
hankst69 <steffen_hanke@web.de>
2024-10-04 20:52:33 +0200

    update build_qt-creator (using choco to setup make)

--- build_choco.bat
+++ build_choco.bat
@@ -2,12 +2,15 @@
 set "_MAKER_ROOT=%~dp0"
 rem https://github.com/chocolatey/choco?tab=readme-ov-file#compiling--building-source
 
-call which choco.exe 1>nul 2>nul
-if %ERRORLEVEL% EQU 0 echo Choco already available &goto :test_choco_success
+call which choco.bat 1>nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+  echo CHOCO already available
+  goto :test_choco_success
+)
 set "_CHOCO_DIR=%_MAKER_ROOT%Choco"
 if not exist "%_CHOCO_DIR%\build.bat" (
   echo.
-  echo 1) clone CHOCO
+  echo 1^) clone CHOCO
   call "%_MAKER_ROOT%\clone_choco.bat"
   rem defines: _CHOCO_DIR
   if "%_CHOCO_DIR%" EQU "" (echo error: cloning CHOCO &goto :EOF)
@@ -15,7 +18,7 @@ if not exist "%_CHOCO_DIR%\build.bat" (
 )
 if not exist "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\choco.exe" (
   echo.
-  echo 2) build CHOCO
+  echo 2^) build CHOCO
   pushd %_CHOCO_DIR%
   call "%_CHOCO_DIR%\build.bat"
   popd
@@ -26,15 +29,20 @@ if not exist "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\choco.exe"
 )
 
 if not exist "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\lib" mkdir "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\lib"
-set "Path=%Path%;%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged"
-call which choco.exe 1>nul 2>nul
+
+set "_TOOLS_DIR=%_MAKER_ROOT%\.tools"
+if not exist "%_TOOLS_DIR%" mkdir "%_TOOLS_DIR%"
+echo @call "%_CHOCO_DIR%\code_drop\temp\_PublishedApps\choco_merged\choco.exe" --allow-unofficial %%*>"%_TOOLS_DIR%\choco.bat"
+call which choco.bat 1>nul 2>nul
+if %ERRORLEVEL% NEQ 0 set "Path=%Path%;%_TOOLS_DIR%"
+call which choco.bat 1>nul 2>nul
 if %ERRORLEVEL% EQU 0 goto :test_choco_success
 echo. error: Choco build seems not to work
 goto :EOF
 
 :test_choco_success
 echo.
-call which choco.exe -l -d
+rem call which choco.bat -l -d
 set _VERSION_NR=
 for /f "tokens=1,2,* delims=-" %%i in ('call choco --version') do set "_VERSION_NR=%%i-%%j-%%k"
 echo choco %_VERSION_NR%
