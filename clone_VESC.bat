b297bf26ad7dbe216ca1564b79f4904cf7499641
hankst69 <steffen_hanke@web.de>
2024-10-07 22:53:12 +0200

    updated build scripts

--- clone_VESC.bat
+++ clone_VESC.bat
@@ -3,11 +3,11 @@ rem https://vesc-project.com/vesc_tool
 rem https://vesc-project.com/node/309
 rem https://github.com/vedderb/vesc_tool
 rem http://github.com/vedderb/bldc
-set "_VESC_DIR=%~dp0VESC"
+set "_MAKER_ROOT=%~dp0"
+
+set "_VESC_DIR=%_MAKER_ROOT%VESC"
 set "_VESC_FW_DIR=%_VESC_DIR%\vesc_fw"
 set "_VESC_TOOL_DIR=%_VESC_DIR%\vesc_tool"
-
-if not exist "%_VESC_DIR%" mkdir "%_VESC_DIR%"
 
 call "%_MAKER_ROOT%\scripts\clone_in_folder.bat" "%_VESC_FW_DIR%" "http://github.com/vedderb/bldc" %*
 call "%_MAKER_ROOT%\scripts\clone_in_folder.bat" "%_VESC_TOOL_DIR%" "https://github.com/vedderb/vesc_tool" %*
