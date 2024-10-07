edff2b51eeaa0cdb65b68d0664dc2c0b7111bde2
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-10-07 09:56:49 +0200

    fix clone_choco.bat

--- clone_choco.bat
+++ clone_choco.bat
@@ -3,4 +3,4 @@ rem https://de.wikipedia.org/wiki/Chocolatey
 rem https://chocolatey.org/
 rem https://github.com/chocolatey/choco.git
 set "_CHOCO_DIR=%~dp0Choco"
-call "%_MAKER_ROOT%\scripts\clone_in_folder.bat" "%_CHOCO_DIR%" "https://github.com/chocolatey/choco.git" --changeDir %*
+call "%~dp0scripts\clone_in_folder.bat" "%_CHOCO_DIR%" "https://github.com/chocolatey/choco.git" --changeDir %*
