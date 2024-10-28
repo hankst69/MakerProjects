de788d68478b283e39770d279564791d4ff6887b
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-10-28 21:10:57 +0100

    more clonings

---
+++ clone_python.bat
@@ -0,0 +1,2 @@
+@call "%~dp0scripts\clone_in_folder.bat" "%~dp0projects\python\examples" "https://https://github.com/hankst69/python_examples" --changeDir
+@call "%~dp0scripts\clone_in_folder.bat" "%~dp0projects\python\Jupyter\SimpleITK-Notebooks" "https://github.com/InsightSoftwareConsortium/SimpleITK-Notebooks.git" --changeDir
