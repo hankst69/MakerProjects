0f1d5c6d9d356e20aaab46de4eda53bb7350fe3d
hankst69 <steffen_hanke@web.de>
2024-10-29 15:13:01 +0100

    simplifications in ensure_msvs scripts

--- scripts/ensure_msvs_matches_python_architecture.bat
+++ scripts/ensure_msvs_matches_python_architecture.bat
@@ -1,33 +1,20 @@
 @echo off
-set "_SCRIPT_ROOT=%~dp0"
-
-rem validate msvs
-call "%_SCRIPT_ROOT%\validate_msvs.bat" 1>nul
-if "%ERRORLEVEL%" equ "0" goto :_msvs_available
-echo error: MSVS not available
-exit /b 1
+set "_SCRIPTS_DIR=%~dp0"
 
 :_msvs_available
-call "%_SCRIPT_ROOT%\validate_python.bat" 1>nul
+call "%_SCRIPTS_DIR%\validate_python.bat" 1>nul
 if "%ERRORLEVEL%" equ "0" goto :_python_available
 echo error: PYTHON not available
-exit /b 2
+exit /b 1
 
 :_python_available
-if /I "%MSVS_TARGET_ARCHITECTURE%" equ "%PYTHON_ARCHITECTURE%" goto :_msvs_matches_python
-
-:msvs_differs_python
-echo warning: MSVS uses target architecture %MSVS_TARGET_ARCHITECTURE% but PYTHON requires %PYTHON_ARCHITECTURE% - switching MSVS
-set _MSVS_TGT_ARCH=x86
-if /I "%MSVS_TARGET_ARCHITECTURE%" equ "x64" set "_MSVS_TGT_ARCH=amd64"
-call vsdevcmd -arch=%_MSVS_TGT_ARCH%
-set _MSVS_TGT_ARCH=
-
-set "MSVS_TARGET_ARCHITECTURE=%VSCMD_ARG_TGT_ARCH%"
-if /I "%MSVS_TARGET_ARCHITECTURE%" equ "%PYTHON_ARCHITECTURE%" goto :_msvs_matches_python
-echo error: MSVS uses target architecture %MSVS_TARGET_ARCHITECTURE% but PYTHON requires %PYTHON_ARCHITECTURE%
-exit /b 3
+rem ensure msvs available and matches
+call "%_SCRIPTS_DIR%\ensure_msvs_matches_architecture.bat" "%PYTHON_ARCHITECTURE%" 1>nul
+if "%ERRORLEVEL%" equ "0" goto :_msvs_matches_python
+echo error: MSVS not available or incompatible
+exit /b 2
 
 :_msvs_matches_python
 echo using: msvs %MSVS_VERSION% (VS%VSCMD_VER:~0,2%) for %MSVS_TARGET_ARCHITECTURE%  (matching python %PYTHON_VERSION% %PYTHON_ARCHITECTURE%)
+set _SCRIPTS_DIR=
 exit /b 0
