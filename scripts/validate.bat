f4af08c0fa29e1d9713fa86bc6cec202f923aded
hankst69 <steffen_hanke@web.de>
2024-11-03 02:04:11 +0100

    added support for msvc validation with architecture requirements

--- scripts/validate.bat
+++ scripts/validate.bat
@@ -15,6 +15,7 @@ goto :EOF
 
 :_start
 @echo off
+call :_clean_script_variables
 set "_VALIDATE_SCRIPT_ROOT=%~dp0"
 set "_VALIDATE_SCRIPT_NAME=_%~n0"
 set "_VALIDATE_NAME=%~1"
@@ -23,18 +24,14 @@ set "_VALIDATE_VERSION_CMD=%3"
 shift
 shift
 shift
-set _VALIDATE_TGT_VERSION=
-set _VALIDATE_TGT_VERSION_COMPARE=
-set _VALIDATE_NO_WARNINGS=
-set _VALIDATE_NO_ERRORS=
-set _VALIDATE_NO_INFO=
-set _VALIDATE_VERBOSE=
-set _VALIDATE_HELP=
 :_param_loop
-rem set "_ARG_TMP_RAW_=%1"
 set "_ARG_TMP_=%~1"
 if "%_ARG_TMP_%" equ "" goto :_param_loop_finish
 shift
+if /I "%_ARG_TMP_%" equ "x86"           (set "_VALIDATE_TGT_ARCHITECTURE=x86" &goto :_param_loop)
+if /I "%_ARG_TMP_%" equ "x64"           (set "_VALIDATE_TGT_ARCHITECTURE=x64" &goto :_param_loop)
+if /I "%_ARG_TMP_%" equ "amd64"         (set "_VALIDATE_TGT_ARCHITECTURE=x64" &goto :_param_loop)
+if /I "%_ARG_TMP_:~0,12%" equ "--tool_arch:" (set "_VALIDATE_TOOL_ARCHITECTURE=%_ARG_TMP_:~12%" &goto :_param_loop)
 if /I "%_ARG_TMP_%" equ "--no_errors"   (set "_VALIDATE_NO_ERRORS=%_ARG_TMP_%" &goto :_param_loop)
 if /I "%_ARG_TMP_%" equ "-ne"           (set "_VALIDATE_NO_ERRORS=--no_errors" &goto :_param_loop)
 if /I "%_ARG_TMP_%" equ "--no_warnings" (set "_VALIDATE_NO_WARNINGS=%_ARG_TMP_%" &goto :_param_loop)
@@ -47,21 +44,16 @@ if /I "%_ARG_TMP_%" equ "--help"        (set "_VALIDATE_HELP=%_ARG_TMP_%" &goto 
 if /I "%_ARG_TMP_%" equ "-h"            (set "_VALIDATE_HELP=--help" &goto :_param_loop)
 if /I "%_ARG_TMP_%" equ "-?"            (set "_VALIDATE_HELP=--help" &goto :_param_loop)
 if '%_ARG_TMP_:~0,1%' equ '-' (echo warning%_VALIDATE_SCRIPT_NAME%: unknown switch '%_ARG_TMP_%' &goto :_param_loop)
-rem if '%_ARG_TMP_RAW_:~0,1%' equ '"' if '%_ARG_TMP_RAW_:~1,1%' equ '-' (echo warning%_VALIDATE_SCRIPT_NAME%: unknown switch '%_ARG_TMP_%' &goto :_param_loop)
-rem if "%_VALIDATE_NAME%"        equ "" set "_VALIDATE_NAME=%_ARG_TMP_%" &goto :_param_loop
-rem if "%_VALIDATE_TEST_CMD%"    equ "" set "_VALIDATE_TEST_CMD=%_ARG_TMP_%" &goto :_param_loop
-rem if "%_VALIDATE_VERSION_CMD:~1,1%" equ "" set "_VALIDATE_VERSION_CMD=%_ARG_TMP_RAW_%" &goto :_param_loop
 if "%_VALIDATE_TGT_VERSION%" equ "" set "_VALIDATE_TGT_VERSION=%_ARG_TMP_%" &goto :_param_loop
 echo warning%_VALIDATE_SCRIPT_NAME%: unknown argument '%_ARG_TMP_%'
 goto :_param_loop
 :_param_loop_finish
 set _ARG_TMP_=
-rem set _ARG_TMP_RAW_=
 
 :_params_postprocessing
-if "%_VALIDATE_NAME%"             equ "" (echo error 1%_VALIDATE_SCRIPT_NAME%: missing arg1: ^<tool_name^> &call :_clean_temp_variables &exit /b 1)
-if "%_VALIDATE_TEST_CMD%"         equ "" (echo error 2%_VALIDATE_SCRIPT_NAME%: missing arg2: ^<test_cmd^>  &call :_clean_temp_variables &exit /b 2)
-if "%_VALIDATE_VERSION_CMD:~1,1%" equ "" (echo error 3%_VALIDATE_SCRIPT_NAME%: missing arg3: ^<version_cmd^> &call :_clean_temp_variables &exit /b 3)
+if "%_VALIDATE_NAME%"             equ "" (echo error 1%_VALIDATE_SCRIPT_NAME%: missing arg1: ^<tool_name^> &call :_clean_script_variables &exit /b 1)
+if "%_VALIDATE_TEST_CMD%"         equ "" (echo error 2%_VALIDATE_SCRIPT_NAME%: missing arg2: ^<test_cmd^>  &call :_clean_script_variables &exit /b 2)
+if "%_VALIDATE_VERSION_CMD:~1,1%" equ "" (echo error 3%_VALIDATE_SCRIPT_NAME%: missing arg3: ^<version_cmd^> &call :_clean_script_variables &exit /b 3)
 rem unquote the cmd string:
 set _VALIDATE_VERSION_CMD=%_VALIDATE_VERSION_CMD:~1,-1%
 call set _VALIDATE_VERSION_CMD=%%_VALIDATE_VERSION_CMD:""="%%
@@ -80,15 +72,32 @@ if /I "%_VALIDATE_TGT_VERSION:~0,3%" equ "LEQ" set "_VALIDATE_TGT_VERSION_COMPAR
 if /I "%_VALIDATE_TGT_VERSION:~0,3%" equ "LEQ" set "_VALIDATE_TGT_VERSION=%_VALIDATE_TGT_VERSION:~3%"
 if /I "%_VALIDATE_TGT_VERSION:~0,3%" equ "LSS" set "_VALIDATE_TGT_VERSION_COMPARE=LSS"
 if /I "%_VALIDATE_TGT_VERSION:~0,3%" equ "LSS" set "_VALIDATE_TGT_VERSION=%_VALIDATE_TGT_VERSION:~3%"
+rem normalize tool architecture:
+if /I "%_VALIDATE_TOOL_ARCHITECTURE%" equ "x86"   set "_VALIDATE_TOOL_ARCHITECTURE=x86"
+if /I "%_VALIDATE_TOOL_ARCHITECTURE%" equ "x64"   set "_VALIDATE_TOOL_ARCHITECTURE=x64"
+if /I "%_VALIDATE_TOOL_ARCHITECTURE%" equ "amd64" set "_VALIDATE_TOOL_ARCHITECTURE=x64"
+rem specific MSVS version handling:
+if /I "%_VALIDATE_NAME%" neq "MSVS" goto :tgt_version_normalized
+rem https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B#Internal_version_numbering
+if "%_VALIDATE_TGT_VERSION%" equ "2022" (set "_VALIDATE_TGT_VERSION=17" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2019" (set "_VALIDATE_TGT_VERSION=16" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2017" (set "_VALIDATE_TGT_VERSION=15" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2015" (set "_VALIDATE_TGT_VERSION=14" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2013" (set "_VALIDATE_TGT_VERSION=12" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2012" (set "_VALIDATE_TGT_VERSION=11" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2010" (set "_VALIDATE_TGT_VERSION=10" &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2008" (set "_VALIDATE_TGT_VERSION=9"  &goto :tgt_version_normalized)
+if "%_VALIDATE_TGT_VERSION%" equ "2005" (set "_VALIDATE_TGT_VERSION=8"  &goto :tgt_version_normalized)
+:tgt_version_normalized
 
 :_params_done
 dir "%~dpn0.---" 1>nul 2>nul
 if %ERRORLEVEL% equ 0 (echo YOUR SHELL IS DEFECT -^> CREATE A NEW ONE &exit /b 99)
 if "%_VALIDATE_VERBOSE%" neq "" call :_validate_verbose_params_list
-if "%_VALIDATE_HELP%" neq "" (call :_usage &call :_clean_temp_variables &goto :EOF)
+if "%_VALIDATE_HELP%" neq "" (call :_usage &call :_clean_script_variables &goto :EOF)
 goto :_execute
 
-:_clean_temp_variables
+:_clean_script_variables
 set _VALIDATE_SCRIPT_ROOT=
 set _VALIDATE_SCRIPT_NAME=
 set _VALIDATE_NAME=
@@ -96,6 +105,8 @@ set _VALIDATE_TEST_CMD=
 set _VALIDATE_VERSION_CMD=
 set _VALIDATE_TGT_VERSION=
 set _VALIDATE_TGT_VERSION_COMPARE=
+set _VALIDATE_TGT_ARCHITECTURE=
+set _VALIDATE_TOOL_ARCHITECTURE=
 set _VALIDATE_NO_WARNINGS=
 set _VALIDATE_NO_ERRORS=
 set _VALIDATE_NO_INFO=
@@ -107,25 +118,26 @@ goto :EOF
 echo _VALIDATE_NAME                : '%_VALIDATE_NAME%'
 echo _VALIDATE_TEST_CMD            : '%_VALIDATE_TEST_CMD%'
 echo _VALIDATE_VERSION_CMD         : '%_VALIDATE_VERSION_CMD%'
-echo _VALIDATE_TGT_VERSION         : '%_VALIDATE_TGT_VERSION%'
-echo _VALIDATE_TGT_VERSION_COMPARE : '%_VALIDATE_TGT_VERSION_COMPARE%'
-echo _VALIDATE_NO_WARNINGS         : '%_VALIDATE_NO_WARNINGS%'
-echo _VALIDATE_NO_ERRORS           : '%_VALIDATE_NO_ERRORS%'
-echo _VALIDATE_NO_INFO             : '%_VALIDATE_NO_INFO%'
-echo _VALIDATE_VERBOSE             : '%_VALIDATE_VERBOSE%'
-echo _VALIDATE_HELP                : '%_VALIDATE_HELP%'
-echo %_VALIDATE_NAME%_VERSION       :
-echo %_VALIDATE_NAME%_VERSION_MAJOR :
-echo %_VALIDATE_NAME%_VERSION_MINOR :
-echo %_VALIDATE_NAME%_VERSION_PATCH :
-echo.
+echo _VALIDATE_TGT_VERSION         : %_VALIDATE_TGT_VERSION%
+echo _VALIDATE_TGT_VERSION_COMPARE : %_VALIDATE_TGT_VERSION_COMPARE%
+echo _VALIDATE_TGT_ARCHITECTURE    : %_VALIDATE_TGT_ARCHITECTURE%
+echo _VALIDATE_NO_WARNINGS         : %_VALIDATE_NO_WARNINGS%
+echo _VALIDATE_NO_ERRORS           : %_VALIDATE_NO_ERRORS%
+echo _VALIDATE_NO_INFO             : %_VALIDATE_NO_INFO%
+echo _VALIDATE_VERBOSE             : %_VALIDATE_VERBOSE%
+echo _VALIDATE_HELP                : %_VALIDATE_HELP%
+rem echo %_VALIDATE_NAME%_VERSION       :
+rem echo %_VALIDATE_NAME%_VERSION_MAJOR :
+rem echo %_VALIDATE_NAME%_VERSION_MINOR :
+rem echo %_VALIDATE_NAME%_VERSION_PATCH :
+rem echo.
 goto :EOF
 
 :_execute
 call %_VALIDATE_TEST_CMD% 1>nul 2>nul
 if %ERRORLEVEL% equ 0 goto :_tool_available
 if "%_VALIDATE_NO_ERRORS%" equ "" echo error 4%_VALIDATE_SCRIPT_NAME%: %_VALIDATE_NAME% not available
-call :_clean_temp_variables
+call :_clean_script_variables
 exit /b 4
 
 :_tool_available
@@ -136,14 +148,14 @@ set /P _VALIDATE_VERSION=<"%TEMP%\_VALIDATE_VERSION_CMD.tmp"
 del /Q /F "%TEMP%\_VALIDATE_VERSION_CMD.tmp" 1>nul 2>nul
 if "%_VALIDATE_VERSION%" neq "" goto :_tool_version_available
 if "%_VALIDATE_NO_ERRORS%" equ "" echo error 5%_VALIDATE_SCRIPT_NAME%: %_VALIDATE_NAME% version unknown
-call :_clean_temp_variables
+call :_clean_script_variables
 exit /b 5
 
 :_tool_version_available
 call "%_VALIDATE_SCRIPT_ROOT%\split_version.bat" "%_VALIDATE_VERSION%" --no_info %_VALIDATE_NO_ERRORS%
 if %ERRORLEVEL% equ 0 goto :_tool_version_split_ok
 if "%_VALIDATE_NO_ERRORS%" equ "" echo error 6%_VALIDATE_SCRIPT_NAME%: %_VALIDATE_NAME% version '%_VALIDATE_VERSION%' not available or invalid
-call :_clean_temp_variables
+call :_clean_script_variables
 exit /b 6
 
 :_tool_version_split_ok
@@ -159,14 +171,22 @@ if "%_VALIDATE_VERBOSE%" neq "" cmd /V:ON /C echo %_VALIDATE_NAME%_VERSION_MINOR
 if "%_VALIDATE_VERBOSE%" neq "" cmd /V:ON /C echo %_VALIDATE_NAME%_VERSION_PATCH : !%_VALIDATE_NAME%_VERSION_PATCH!
 
 :_tool_version_requirement_test
-if "%_VALIDATE_TGT_VERSION%" equ "" goto :_tool_validation_success
+if "%_VALIDATE_TGT_VERSION%" equ "" goto :_tool_architecture_requirement_test
 call "%_VALIDATE_SCRIPT_ROOT%\compare_versions.bat" --no_info %_VALIDATE_NO_ERRORS% "%_VALIDATE_VERSION%" "%_VALIDATE_TGT_VERSION%" "%_VALIDATE_TGT_VERSION_COMPARE%"
-if %ERRORLEVEL% equ 0 goto :_tool_validation_success
+if %ERRORLEVEL% equ 0 goto :_tool_architecture_requirement_test
 if "%_VALIDATE_NO_ERRORS%" equ "" echo error 7%_VALIDATE_SCRIPT_NAME%: %_VALIDATE_NAME% version '%_VALIDATE_VERSION%' does not match required version '%_VALIDATE_TGT_VERSION%'
-call :_clean_temp_variables
+call :_clean_script_variables
 exit /b 7
 
+:_tool_architecture_requirement_test
+if "%_VALIDATE_TGT_ARCHITECTURE%" equ "" goto :_tool_validation_success
+if "%_VALIDATE_TOOL_ARCHITECTURE%" equ "" goto :_tool_validation_success
+if /I "%_VALIDATE_TOOL_ARCHITECTURE%" equ "%_VALIDATE_TGT_ARCHITECTURE%" goto :_tool_validation_success
+if "%_VALIDATE_NO_ERRORS%" equ "" echo error 8%_VALIDATE_SCRIPT_NAME%: %_VALIDATE_NAME% architecture '%_VALIDATE_TOOL_ARCHITECTURE%' does not match required type '%_VALIDATE_TGT_ARCHITECTURE%'
+call :_clean_script_variables
+exit /b 8
+
 :_tool_validation_success
-if "%_VALIDATE_NO_INFO%" equ "" cmd /Q /V:ON /C echo using: %_VALIDATE_NAME% !%_VALIDATE_NAME%_VERSION!
-call :_clean_temp_variables
+if "%_VALIDATE_NO_INFO%" equ "" cmd /Q /V:ON /C echo using: %_VALIDATE_NAME% !%_VALIDATE_NAME%_VERSION! %_VALIDATE_TOOL_ARCHITECTURE%
+call :_clean_script_variables
 exit /b 0
--- scripts/validate_msvs.bat
+++ scripts/validate_msvs.bat
@@ -1,66 +1,16 @@
+@rem validate msvs:
 @echo off
-rem validate msvs
-set "_SCRIPT_ROOT=%~dp0"
-set MSVS_TARGET_ARCHITECTURE=
 set MSVS_YEAR=
 set MSVS_VERSION=
 set MSVS_VERSION_MAJOR=
 set MSVS_VERSION_MINOR=
 set MSVS_VERSION_PATCH=
+set MSVS_TARGET_ARCHITECTURE=
 
-set _MSVS_TGT_ARCHITECTURE=
-set _MSVS_TGT_VERSION=
-set _MSVS_TGT_VERSION_COMPARE=
-set _MSVS_NO_WARNINGS=
-set _MSVS_NO_ERRORS=
-set _MSVS_NO_INFO=
-:param_loop
-if /I "%~1" equ "x86"   (set "_MSVS_TGT_ARCHITECTURE=x86" &shift &goto :param_loop)
-if /I "%~1" equ "x64"   (set "_MSVS_TGT_ARCHITECTURE=x64" &shift &goto :param_loop)
-if /I "%~1" equ "amd64" (set "_MSVS_TGT_ARCHITECTURE=x64" &shift &goto :param_loop)
-if /I "%~1" equ "--no_warnings" (set "_MSVS_NO_WARNINGS=%~1" &shift &goto :param_loop)
-if /I "%~1" equ "--no_errors"   (set "_MSVS_NO_ERRORS=%~1" &shift &goto :param_loop)
-if /I "%~1" equ "--no_info"     (set "_MSVS_NO_INFO=%~1" &shift &goto :param_loop)
-if "%~1" neq "" if "%_MSVS_TGT_VERSION%" equ "" (set "_MSVS_TGT_VERSION=%~1" &shift &goto :param_loop)
-if "%~1" neq "" (echo warning: unknown argument '%~1' &shift &goto :param_loop)
+call "%~dp0\validate.bat" "MSVS" "msbuild -version" "echo %VSCMD_VER%" "--tool_arch:%VSCMD_ARG_TGT_ARCH%" %*
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-if "%_MSVS_TGT_VERSION%" equ "" goto :validate_msvs
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "GEQ" set "_MSVS_TGT_VERSION_COMPARE=GEQ"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "GEQ" set "_MSVS_TGT_VERSION=%_MSVS_TGT_VERSION:~3%"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "GTR" set "_MSVS_TGT_VERSION_COMPARE=GTR"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "GTR" set "_MSVS_TGT_VERSION=%_MSVS_TGT_VERSION:~3%"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "LEQ" set "_MSVS_TGT_VERSION_COMPARE=LEQ"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "LEQ" set "_MSVS_TGT_VERSION=%_MSVS_TGT_VERSION:~3%"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "LSS" set "_MSVS_TGT_VERSION_COMPARE=LSS"
-if /I "%_MSVS_TGT_VERSION:~0,3%" equ "LSS" set "_MSVS_TGT_VERSION=%_MSVS_TGT_VERSION:~3%"
-
-rem https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B#Internal_version_numbering
-if "%_MSVS_TGT_VERSION%" equ "2022" (set "_MSVS_TGT_VERSION=17" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2019" (set "_MSVS_TGT_VERSION=16" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2017" (set "_MSVS_TGT_VERSION=15" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2015" (set "_MSVS_TGT_VERSION=14" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2013" (set "_MSVS_TGT_VERSION=12" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2012" (set "_MSVS_TGT_VERSION=11" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2010" (set "_MSVS_TGT_VERSION=10" &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2008" (set "_MSVS_TGT_VERSION=9"  &goto :tgt_version_normalized)
-if "%_MSVS_TGT_VERSION%" equ "2005" (set "_MSVS_TGT_VERSION=8"  &goto :tgt_version_normalized)
-:tgt_version_normalized
-
-
-:validate_msvs
 set "MSVS_TARGET_ARCHITECTURE=%VSCMD_ARG_TGT_ARCH%"
-set "MSVS_YEAR="
-set "MSVS_VERSION=%VSCMD_VER%"
-call "%_SCRIPT_ROOT%split_version.bat" "%MSVS_VERSION%" 1>NUL
-if "%ERRORLEVEL%" equ "0" goto :split_msvs_version_ok
-if "%_MSVS_NO_ERRORS%" equ "" echo error: MSVS version '%MSVS_VERSION%' not available or invalid
-exit /b 2
-
-:split_msvs_version_ok
-set "MSVS_VERSION_MAJOR=%VERSION_MAJOR%"
-set "MSVS_VERSION_MINOR=%VERSION_MINOR%"
-set "MSVS_VERSION_PATCH=%VERSION_PATCH%"
-
 if /I "%MSVS_VERSION_MAJOR%" equ "17" (set "MSVS_YEAR=2022" &goto :test_msvs_version)
 if /I "%MSVS_VERSION_MAJOR%" equ "16" (set "MSVS_YEAR=2019" &goto :test_msvs_version)
 if /I "%MSVS_VERSION_MAJOR%" equ "15" (set "MSVS_YEAR=2017" &goto :test_msvs_version)
@@ -70,31 +20,10 @@ if /I "%MSVS_VERSION_MAJOR%" equ "11" (set "MSVS_YEAR=2012" &goto :test_msvs_ver
 if /I "%MSVS_VERSION_MAJOR%" equ "10" (set "MSVS_YEAR=2010" &goto :test_msvs_version)
 if /I "%MSVS_VERSION_MAJOR%" equ "9"  (set "MSVS_YEAR=2008" &goto :test_msvs_version)
 if /I "%MSVS_VERSION_MAJOR%" equ "8"  (set "MSVS_YEAR=2005" &goto :test_msvs_version)
-if "%_MSVS_NO_ERRORS%" equ "" echo error: MSVS not available (unexpected major version '%MSVS_VERSION_MAJOR%')
-exit /b 3
+if "%_MSVS_NO_ERRORS%" equ "" echo error 88: MSVS not available (unexpected major version '%MSVS_VERSION_MAJOR%')
+exit /b 88
 
 :test_msvs_version
-if "%_MSVS_TGT_VERSION%" equ "" goto :test_msvs_tgt_architecture
-call "%_SCRIPT_ROOT%compare_versions.bat" "%MSVS_VERSION%" "%_MSVS_TGT_VERSION%" "%_MSVS_TGT_VERSION_COMPARE%" --no_info
-if "%ERRORLEVEL%" equ "0" goto :test_msvs_tgt_architecture
-if "%_MSVS_NO_ERRORS%" equ "" echo error: MSVS version '%MSVS_VERSION%' does not match required version '%_MSVS_TGT_VERSION%'
-exit /b 4
-
-:test_msvs_tgt_architecture
-if "%_MSVS_TGT_ARCHITECTURE%" equ "" goto :test_msvs_success
-if /I "%MSVS_TARGET_ARCHITECTURE%" equ "%_MSVS_TGT_ARCHITECTURE%" goto :test_msvs_success
-if "%_MSVS_NO_ERRORS%" equ "" echo error: MSVS target architecture '%MSVS_TARGET_ARCHITECTURE%' does not match required type '%_MSVS_TGT_ARCHITECTURE%'
-exit /b 5
-
-
 :test_msvs_success
-if "%_MSVS_NO_INFO%" equ "" echo using: msvs %MSVS_YEAR% (vs %MSVS_VERSION%) for %MSVS_TARGET_ARCHITECTURE%
-
-set _MSVS_TGT_ARCHITECTURE=
-set _MSVS_TGT_VERSION=
-set _MSVS_TGT_VERSION_COMPARE=
-set _MSVS_NO_WARNINGS=
-set _MSVS_NO_ERRORS=
-set _MSVS_NO_INFO=
-set _SCRIPT_ROOT=
-exit /b 0
+rem if "%_MSVS_NO_INFO%" equ "" echo using: msvs %MSVS_YEAR% (vs %MSVS_VERSION%) for %MSVS_TARGET_ARCHITECTURE%
+goto :EOF
