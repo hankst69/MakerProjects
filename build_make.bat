bd3621918785924e45c05dc75882e0cb4addc8ed
hankst69 <steffen_hanke@web.de>
2024-11-06 12:27:21 +0100

    fix build_make

--- build_make.bat
+++ build_make.bat
@@ -6,12 +6,12 @@ set "_BMK_START_DIR=%cd%"
 call "%MAKER_SCRIPTS%\validate_make.bat" 1>nul
 if %ERRORLEVEL% EQU 0 goto :test_make_success
 
-if exist "%%MAKER_BIN%%\make.bat" (
+if exist "%MAKER_BIN%\make.bat" (
   rem deep test for correct version
   set _VERSION_NR=
   for /f "tokens=1,2,* delims= " %%i in ('call "%%MAKER_BIN%%\make.bat" --version') do if /I "%%i" equ "gnu" if /I "%%j" equ "make" set "_VERSION_NR=%%k"
   rem if "%_VERSION_NR%" neq "" echo MAKE already available
-  if "%_VERSION_NR%" neq "" set "Path=%%MAKER_BIN%%;%Path%"
+  if "%_VERSION_NR%" neq "" set "Path=%MAKER_BIN%;%Path%"
   if "%_VERSION_NR%" neq "" goto :test_make_success
 )
 
@@ -27,10 +27,10 @@ if not exist "%_CHOCO_BIN%\bin\make.exe" (
   )
 )
 
-echo @call "%%_CHOCO_BIN%%\bin\make.exe" %%* >"%%MAKER_BIN%%\make.bat"
+echo @call "%%_CHOCO_BIN%%\bin\make.exe" %%* >"%MAKER_BIN%\make.bat"
 
 call "%MAKER_SCRIPTS%\validate_make.bat" 1>nul
-if %ERRORLEVEL% NEQ 0 set "Path=%%MAKER_BIN%%;%Path%"
+if %ERRORLEVEL% NEQ 0 set "Path=%MAKER_BIN%;%Path%"
 
 
 call "%MAKER_SCRIPTS%\validate_make.bat" 1>nul
@@ -43,5 +43,5 @@ if %ERRORLEVEL% NEQ 0 (
 call "%MAKER_SCRIPTS%\validate_make.bat"
 
 :exit_script
-cd /d "_BMK_START_DIR"
+cd /d "%_BMK_START_DIR%"
 set _BMK_START_DIR=
