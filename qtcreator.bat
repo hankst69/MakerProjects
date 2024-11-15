0e5b35f22a0a2aa662b68a6a4a5da0e1073c81ed
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-11-15 14:36:38 +0100

    support for qtdesigner and qtcreator

---
+++ qtcreator.bat
@@ -0,0 +1,1 @@
+@call "%~dp0\build_qt-creator.bat" --start
---
+++ qtdesigner.bat
@@ -0,0 +1,4 @@
+@rem call "%~dp0\maker_env.bat"
+@rem call "%~dp0\build_qt-designer.bat"
+@call "%~dp0\build_qt.bat"
+@call "%MAKER_BIN%\qtdesigner.bat"
