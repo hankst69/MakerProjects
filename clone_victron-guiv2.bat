035ebd9a189ea039f7d5206260bae8e069e2de04
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-11-20 09:09:37 +0100

    cloning submodules in victron-gui-v2

--- clone_victron-guiv2.bat
+++ clone_victron-guiv2.bat
@@ -7,3 +7,4 @@ set "_VICTRON_GUIV2_DIR=%_VICTRON_DIR%\gui-v2"
 rem if not exist "%_VICTRON_DIR%" mkdir "%_VICTRON_DIR%"
 
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_VICTRON_GUIV2_DIR%" "https://github.com/victronenergy/gui-v2.git" --changeDir
+call git submodule update --init
