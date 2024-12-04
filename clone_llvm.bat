dd66f45197989df2c01ea5c98b52e8705f5bc211
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-12-04 22:53:13 +0100

    update llvm build

--- clone_llvm.bat
+++ clone_llvm.bat
@@ -12,7 +12,7 @@ rem --- cloning LLVM
 :llvm_clone
 rem if not exist "%_LLVM_DIR%" mkdir "%_LLVM_DIR%"
 rem if not exist "%_LLVM_SOURCES_DIR%" mkdir "%_LLVM_SOURCES_DIR%"
-if exist "%_LLVM_SOURCES_DIR%\cmake\configure.bat" goto :llvm_clone_done
+if exist "%_LLVM_SOURCES_DIR%\cmake\Modules\LLVMVersion.cmake" goto :llvm_clone_done
 
 rem call "%_ROOT_DIR%scripts\clone_in_folder.bat" "%_LLVM_SOURCES_DIR%" "https://github.com/llvm/llvm-project.git" --depth 1 --config core.autocrlf=false --changeDir
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_LLVM_SOURCES_DIR%" "https://github.com/llvm/llvm-project.git" --depth 1 --changeDir
