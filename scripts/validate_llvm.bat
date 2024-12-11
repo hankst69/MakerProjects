64395ee3cc2c50db30ef4c8444cb811ff039bba6
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2024-12-11 11:09:12 +0100

    update in build and validate llvm

--- scripts/validate_llvm.bat
+++ scripts/validate_llvm.bat
@@ -1,6 +1,2 @@
-@echo off
-rem validate llvm (set LLVM_INSTALL_DIR + need to set the FEATURE_clang and FEATURE_clangcpp CMake variable to ON to re-evaluate this checks)
-rem ...tbd
-call clang --version 1>nul 2>nul
-if %ERRORLEVEL% equ 0 exit /b 0
-exit /b 1
+@rem validate llvm (set LLVM_INSTALL_DIR + need to set the FEATURE_clang and FEATURE_clangcpp CMake variable to ON to re-evaluate this checks)
+@call "%~dp0\validate.bat" "LLVM-CLANG" "clang --version" "for /f ""tokens=2,3 delims= "" %%%%i in ('call clang --version') do if ""%%%%i"" equ ""version"" for /f ""tokens=1,* delims=g"" %%%%k in (""%%%%j"") do echo %%%%k" %*
