539e4a42be8bcedd807c36de8228c398bb139e92
hankst69 <steffen_hanke@web.de>
2024-12-15 13:45:24 +0100

    update LLVM-Clang build

--- build_llvm.bat
+++ build_llvm.bat
@@ -14,7 +14,8 @@ if /I "%~1" equ "Debug"        (set "_LLVM_BUILD_TYPE=%~1" &shift &goto :param_l
 if /I "%~1" equ "Release"      (set "_LLVM_BUILD_TYPE=%~1" &shift &goto :param_loop)
 rem if /I "%~1" equ "RelWithDebug" (set "_LLVM_BUILD_TYPE=%~1" &shift &goto :param_loop)
 if "%~1" neq "" if "%_LLVM_VERSION%" equ "" (set "_LLVM_VERSION=%~1" &shift &goto :param_loop)
-if "%~1" neq ""              (echo error: unknonwn argument '%~1' &goto :EOF)
+if "%~1" neq "" (echo error: unknown argument '%~1' &goto :EOF)
+
 
 rem (1) *** cloning LLVM sources ***
 rem defines: _LLVM_DIR
@@ -28,14 +29,16 @@ if not exist "%_LLVM_SOURCES_DIR%" (echo cloning LLVM %_LLVM_VERSION% failed &go
 set "_LLVM_BUILD_DIR=%_LLVM_DIR%\llvm_build%_LLVM_VERSION%"
 set "_LLVM_BIN_DIR=%_LLVM_DIR%\llvm%_LLVM_VERSION%"
 
-rem (2) *** cleaning QT build if demanded ***
+
+rem (2) *** removing prior build outputs ***
 if "%_REBUILD%" equ "true" (
   echo preparing rebuild...
   rmdir /s /q "%_LLVM_BIN_DIR%" 1>nul 2>nul
   rmdir /s /q "%_LLVM_BUILD_DIR%" 1>nul 2>nul
 )
 
-rem (4) *** ensuring prerequisites ***
+
+rem (3) *** ensuring prerequisites ***
 echo.
 echo rebuilding LLVM %_LLVM_VERSION% from sources
 echo see https://llvm.org/docs/GettingStarted.html#getting-the-source-code-and-building-llvm
@@ -59,22 +62,24 @@ if not exist "%_LLVM_BIN_DIR%" mkdir "%_LLVM_BIN_DIR%"
 if not exist "%_LLVM_BUILD_DIR%" mkdir "%_LLVM_BUILD_DIR%"
 
 
-rem (5) *** perform LLVM Cmake configuration ***
+rem (4) *** perform cmake configuration ***
 :_configure
 if exist "%_LLVM_BUILD_DIR%\lib\Analysis\LLVMAnalysis.dir\%_LLVM_BUILD_TYPE%\AliasAnalysis.obj" goto :_configure_done
 echo.
-echo LLVM-CONFIGURATION %_LLVM_VERSION% (Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR% x64)
+echo LLVM-CONFIGURATION %_LLVM_VERSION% (%_LLVM_BUILD_TYPE% %_LLVM_TGT_ARCH%)
 echo using generator "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%"
-rem cmake -S <source_dir> -B <build_dir> -G <generator> [options] -A <architecture>
+rem cmake -S <source_dir> -B <build_dir> -G <generator> [options] -A <architecture> [--trace] [--fresh] [-DCMAKE_BUILD_TYPE=Release] [ -DCMAKE_INSTALL_PREFIX="<install_folder>"]
+rem for multiconfig generators like "Visual Studio ..." the BuildType option is ignored and needs to be specified at build time again
+rem
 rem cmake -S D:\GIT\Maker\tools\LLVM\llvm-project\llvm -B D:\GIT\Maker\tools\LLVM\llvm_build -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release --install-prefix D:\GIT\Maker\tools\LLVM\llvm --trace --fresh
-rem echo cmake -S "%_LLVM_SOURCES_DIR%\llvm" -B "%_LLVM_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_LLVM_TGT_ARCH% -DCMAKE_BUILD_TYPE="%_LLVM_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX="%_LLVM_BIN_DIR%" -DLLVM_ENABLE_PROJECTS="clang;lld;"
+rem
 echo cmake -S "%_LLVM_SOURCES_DIR%\llvm" -B "%_LLVM_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_LLVM_TGT_ARCH% --install-prefix "%_LLVM_BIN_DIR%" -DLLVM_ENABLE_PROJECTS="clang;lld;" -DCMAKE_BUILD_TYPE="%_LLVM_BUILD_TYPE%"
 call cmake -S "%_LLVM_SOURCES_DIR%\llvm" -B "%_LLVM_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_LLVM_TGT_ARCH% --install-prefix "%_LLVM_BIN_DIR%" -DLLVM_ENABLE_PROJECTS="clang;lld;" -DCMAKE_BUILD_TYPE="%_LLVM_BUILD_TYPE%"
 :_configure_done
 echo LLVM-CONFIGURE %_LLVM_VERSION% done
 
 
-rem (6) *** perform LLVM build ***
+rem (5) *** perform build ***
 :_build
 if exist "%_LLVM_BUILD_DIR%\%_LLVM_BUILD_TYPE%\bin\clang.exe" goto :_build_done
 echo.
@@ -85,7 +90,7 @@ call cmake --build . --parallel 4 --config %_LLVM_BUILD_TYPE%
 echo LLVM-BUILD %_LLVM_VERSION% done
 
 
-rem (7) *** perform LLVM install ***
+rem (7) *** perform install ***
 :_install
 if exist "%_LLVM_BIN_DIR%\bin\clang.exe" goto :_install_done
   echo.
