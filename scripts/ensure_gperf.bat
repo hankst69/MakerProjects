4f03d7179cab3608dc617afe55f469c75a9e7752
hankst69 <steffen_hanke@web.de>
2024-12-15 21:48:57 +0100

    fixes for building GPERF and NODEJS

--- scripts/ensure_gperf.bat
+++ scripts/ensure_gperf.bat
@@ -1,17 +1,14 @@
 @echo off
 call "%~dp0\maker_env.bat"
 call "%MAKER_SCRIPTS%\validate_gperf.bat" %* 1>nul 2>nul
-if %ERRORLEVEL% EQU 0 echo GPERF %* available &exit /b 0
+if %ERRORLEVEL% EQU 0 echo GPERF %GPERF_VERSION% available &exit /b 0
 
 set "_GP_BIN_DIR=%MAKER_TOOLS%\GPerf\gperf\bin"
 set "_GP_TEST_OBJECT=%_GP_BIN_DIR%\tcmalloc_minimal.dll"
+
 if not exist "%_GP_TEST_OBJECT%" call "%MAKER_ROOT%\build_gperf.bat" %*
 if not exist "%_GP_TEST_OBJECT%" echo error: GPERF %* failed &exit /b 1
-rem echo %PATH%
-if exist "%_GP_TEST_OBJECT%" set "PATH=%PATH%;%_GP_BIN_DIR%"
-rem echo %PATH%
 
-call "%MAKER_SCRIPTS%\validate_gperf.bat" %*
-if %ERRORLEVEL% EQU 0 echo GPERF %* available &exit /b 0
-echo error: GPERF %* failed
-exit /b 1
+if exist "%_GP_TEST_OBJECT%" set "PATH=%PATH%;%_GP_BIN_DIR%"
+
+"%MAKER_SCRIPTS%\validate_gperf.bat" %*
--- scripts/ensure_nodejs.bat
+++ scripts/ensure_nodejs.bat
@@ -1,15 +1,13 @@
 @echo off
 call "%~dp0\maker_env.bat"
 call "%MAKER_SCRIPTS%\validate_nodejs.bat" %* 1>nul 2>nul
-if %ERRORLEVEL% EQU 0 echo NODE.JS %* available &exit /b 0
+if %ERRORLEVEL% EQU 0 goto :Exit
 
 set "_NODE_BIN_DIR=%VSINSTALLDIR%\MSBuild\Microsoft\VisualStudio\NodeJs"
 set "_NODE_TEST_OBJECT=%_NODE_BIN_DIR%\node.exe"
 rem if not exist "%_NODE_TEST_OBJECT%" call %MAKER_ROOT%\build_nodejs.bat" %*
-if not exist "%_NODE_TEST_OBJECT%" echo error: NODE.JS %* failed &exit /b 1
+if not exist "%_NODE_TEST_OBJECT%" echo error: NODEJS failed &exit /b 1
 if exist "%_NODE_TEST_OBJECT%" set "PATH=%PATH%;%_NODE_BIN_DIR%"
 
-call "%MAKER_SCRIPTS%\validate_nodejs.bat" %* 1>nul 2>nul
-if %ERRORLEVEL% EQU echo NODE.JS %* available &exit /b 0
-echo error: NODE.JS %* failed
-exit /b 1
+:Exit
+"%MAKER_SCRIPTS%\validate_nodejs.bat" %*
