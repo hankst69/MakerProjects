2b5bd122f1096ebca9b582a0d1b4a14becb988c0
hankst69 <steffen_hanke@web.de>
2024-12-30 19:09:05 +0100

    minor fix in qt and emsdk build

--- scripts/clone_emsdk.bat
+++ scripts/clone_emsdk.bat
@@ -8,7 +8,7 @@ rem if "%_EMSDK_VERSION%"  equ "" set _EMSDK_VERSION=1.38.45
 rem if "%_EMSDK_VERSION%"  equ "" set "_EMSDK_VERSION=3.1.67" &rem (current latest as of 2024/09)
 if "%_EMSDK_VERSION%"  equ "" set _EMSDK_VERSION=latest
 
-set "_EMSDK_DIR=%MAKER_TOOLS%\Emsdk"
+set "_EMSDK_DIR=%MAKER_TOOLS%\EMSDK"
 set "_EMSDK_BIN_DIR=%_EMSDK_DIR%\%_EMSDK_VERSION%"
 
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_EMSDK_BIN_DIR%" "https://github.com/emscripten-core/emsdk.git" --changeDir
