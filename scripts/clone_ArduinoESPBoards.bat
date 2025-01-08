2a4540321af954b8b85437f655f2478f023a7115
hankst69 <steffen_hanke@web.de>
2025-01-08 19:21:50 +0100

    updated clone scripts

--- scripts/clone_ArduinoESPBoards.bat
+++ scripts/clone_ArduinoESPBoards.bat
@@ -8,3 +8,4 @@ set "_ARDUINO_ESP32_DIR=%MAKER_TOOLS%\ArduinoESP32"
 
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_ARDUINO_ESP8266_DIR%" "https://github.com/esp8266/Arduino.git" --changeDir
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_ARDUINO_ESP32_DIR%" "https://github.com/espressif/arduino-esp32" --changeDir
+rem cd /d "%MAKER_TOOLS%"
