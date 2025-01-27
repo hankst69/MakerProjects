bf3aa22d637b965993c91e4935403698b47fc8a1
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-01-27 19:27:05 +0100

    updates in build: qt, victron, gammaray

--- scripts/build_gammaray.bat
+++ scripts/build_gammaray.bat
@@ -4,11 +4,12 @@
 set "_BGR_START_DIR=%cd%"
 
 call "%~dp0\maker_env.bat" %*
+call "%MAKER_SCRIPTS%\clear_temp_envs.bat" "_GR_"
 
 set "_GR_VERSION=%MAKER_ENV_VERSION%"
 set "_GT_BUILD_TYPE=%MAKER_ENV_BUILDTYPE%"
 set "_GR_TGT_ARCH=%MAKER_ENV_ARCHITECTURE%"
-set "_REBUILD=%MAKER_ENV_REBUILD%"
+set "_GR_REBUILD=%MAKER_ENV_REBUILD%"
 
 rem apply defaults
 if "%_GR_TGT_ARCH%" equ "" set "_GR_TGT_ARCH=x64"
@@ -19,22 +20,23 @@ set _GR_TGT_ARCH=x64
 rem define target QT framework and build system
 rem defaults:
 set _GR_MSVS_VERSION=GEQ2019
-set _GR_CMAKE_VERSION=GEQ3.16
+set _GR_CMAKE_VERSION=GEQ3.22
 set _GR_QT_VERSION=GEQ6.3
 rem if we build GammaRay 3.0 this means that we like to meet the ChimaeraCUT3.6SDK Qt version from C:\Chimaera\CUT.SDK-3.6.0\bin\Release
 if "%_GR_VERSION%" equ "3.0" set _GR_QT_VERSION=15.12
-if "%_GR_VERSION%" equ "3.0" set _GR_MSVS_VERSION=2019
-if "%_GR_VERSION%" equ "3.0" set _GR_CMAKE_VERSION=GEQ3.22
+rem if "%_GR_VERSION%" equ "3.0" set _GR_MSVS_VERSION=2019
+rem if "%_GR_VERSION%" equ "3.0" set _GR_CMAKE_VERSION=GEQ3.22
+
 
 rem *** cloning sources ***
 call "%MAKER_BUILD%\clone_gammaray.bat" %_GR_VERSION% %MAKER_ENV_VERBOSE% --silent
 cd /d "%_BGR_START_DIR%"
 rem defines: _GR_DIR
 rem defines: _GR_SOURCES_DIR
-if "%_GR_DIR%" EQU "" (echo cloning GammaRay %_GR_VERSION% failed &goto :Exit)
-if "%_GR_SOURCES_DIR%" EQU "" (echo cloning GammaRay %_GR_VERSION% failed &goto :Exit)
-if not exist "%_GR_DIR%" (echo cloning GammaRay %_GR_VERSION% failed &goto :Exit)
-if not exist "%_GR_SOURCES_DIR%" (echo cloning GammaRay %_GR_VERSION% failed &goto :Exit)
+if "%_GR_DIR%" EQU "" (echo cloning GammaRay %_GR_VERSION% failed &goto :_exit)
+if "%_GR_SOURCES_DIR%" EQU "" (echo cloning GammaRay %_GR_VERSION% failed &goto :_exit)
+if not exist "%_GR_DIR%" (echo cloning GammaRay %_GR_VERSION% failed &goto :_exit)
+if not exist "%_GR_SOURCES_DIR%" (echo cloning GammaRay %_GR_VERSION% failed &goto :_exit)
 
 set "_GR_BUILD_DIR=%_GR_DIR%\GammaRay_build%_GR_VERSION%"
 set "_GR_BIN_DIR=%_GR_DIR%\GammaRay%_GR_VERSION%"
@@ -43,7 +45,7 @@ if "%MAKER_ENV_VERBOSE%" neq "" set _GR
 
 
 rem *** cleaning GR build if demanded ***
-if "%_REBUILD%" neq "" (
+if "%_GR_REBUILD%" neq "" (
   echo preparing rebuild...
   rmdir /s /q "%_GR_BIN_DIR%" 1>nul 2>nul
   rmdir /s /q "%_GR_BUILD_DIR%" 1>nul 2>nul
@@ -51,30 +53,30 @@ if "%_REBUILD%" neq "" (
 
 
 rem *** testing for existing GR build ***
-if not exist "%_GR_BIN_DIR%\bin\gammaray.exe" goto :build_gr
+if not exist "%_GR_BIN_DIR%\bin\gammaray.exe" goto :_rebuild
 call "%MAKER_BUILD%\validate_gammaray.bat" 1>nul
 if %ERRORLEVEL% EQU 0 (
   echo GAMMARAY %_GR_VERSION% already available
-  goto :gr_install_done
+  goto :_install_done
 )
 if %ERRORLEVEL% EQU 4 set "PATH=%_GR_BIN_DIR%\bin;%PATH%"
 call "%MAKER_BUILD%\validate_gammaray.bat" 1>nul
 if %ERRORLEVEL% EQU 0 (
   echo GAMMARAY %_GR_VERSION% already available
-  goto :gr_install_done
+  goto :_install_done
 )
 call "%MAKER_BUILD%\build_qt.bat"
 call "%MAKER_BUILD%\validate_gammaray.bat" 1>nul
 if %ERRORLEVEL% EQU 0 (
   echo GAMMARAY %_GR_VERSION% already available
-  goto :gr_install_done
+  goto :_install_done
 )
 echo error: GAMMARAY %_GR_VERSION% seems to be prebuild but is not working
 echo try rebuilding via '%~n0 --rebuild %_GR_VERSION%'
-goto :Exit
+goto :_exit
 
 
-:build_gr
+:_rebuild
 rem *** ensuring prerequisites ***
 
 rem https://github.com/KDAB/GammaRay/blob/master/INSTALL.md
@@ -97,32 +99,32 @@ echo.
 rem ensure msvs version and amd64 target architecture
 call "%MAKER_BUILD%\ensure_msvs.bat" %_GR_MSVS_VERSION% amd64 %MAKER_ENV_VERBOSE%
 if %ERRORLEVEL% NEQ 0 (
-  goto :Exit
+  goto :_exit
 )
 rem validate cmake
 call "%MAKER_BUILD%\validate_cmake.bat" %_GR_CMAKE_VERSION% %MAKER_ENV_VERBOSE%
 if %ERRORLEVEL% NEQ 0 (
-  goto :Exit
+  goto :_exit
 )
 rem validate ninja
 call "%MAKER_BUILD%\validate_ninja.bat" --no_errors %MAKER_ENV_VERBOSE%
 if %ERRORLEVEL% NEQ 0 (
   echo warning: NINJA is not available
-  rem goto :Exit
+  rem goto :_exit
 )
 rem ensure qt
 call "%MAKER_BUILD%\validate_qt.bat" %_GR_QT_VERSION% %MAKER_ENV_VERBOSE%
-if %ERRORLEVEL% EQU 0 goto :gr_configure
+if %ERRORLEVEL% EQU 0 goto :_configure
 if "%_GR_VERSION%" neq "3.0" call "%MAKER_BUILD%\build_qt.bat"
 if "%_GR_VERSION%" equ "3.0" call "%MAKER_BUILD%\build_qt.bat" %_GR_QT_VERSION%
 call "%MAKER_BUILD%\validate_qt.bat" %_GR_QT_VERSION% %MAKER_ENV_VERBOSE%
 if %ERRORLEVEL% NEQ 0 (
-  goto :Exit
+  goto :_exit
 )
 
 
 rem *** configure GR build ***
-:gr_configure
+:_configure
 echo.
 echo GAMMARAY-CONFIGURE %_GR_VERSION% (%_GR_BUILD_TYPE%)
 rmdir /s /q "%_GR_BUILD_DIR%" 1>nul 2>nul
@@ -132,50 +134,52 @@ mkdir "%_GR_BUILD_DIR%"
 rem pushd "%_GR_BUILD_DIR%"
 rem call cmake -S "%_GR_SOURCES_DIR%" -G Ninja -DCMAKE_INSTALL_PREFIX="%_GR_BIN_DIR%" .
 rem popd
-echo cmake -S "%_GR_SOURCES_DIR%" -B "%_GR_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_GR_TGT_ARCH% -DCMAKE_BUILD_TYPE="%_GR_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX="%_GR_BIN_DIR%" -DCMAKE_PREFIX_PATH="%_QT_BIN_DIR%"
-call cmake -S "%_GR_SOURCES_DIR%" -B "%_GR_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_GR_TGT_ARCH% -DCMAKE_BUILD_TYPE="%_GR_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX="%_GR_BIN_DIR%" -DCMAKE_PREFIX_PATH="%_QT_BIN_DIR%"
-:gr_configure_done
+echo cmake -S "%_GR_SOURCES_DIR%" -B "%_GR_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_GR_TGT_ARCH% -DCMAKE_BUILD_TYPE="%_GR_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX="%_GR_BIN_DIR%" -DCMAKE_PREFIX_PATH="%QT_BIN_DIR%"
+call cmake -S "%_GR_SOURCES_DIR%" -B "%_GR_BUILD_DIR%" -G "Visual Studio %MSVS_VERSION_MAJOR% %MSVS_YEAR%" -A %_GR_TGT_ARCH% -DCMAKE_BUILD_TYPE="%_GR_BUILD_TYPE%" -DCMAKE_INSTALL_PREFIX="%_GR_BIN_DIR%" -DCMAKE_PREFIX_PATH="%QT_BIN_DIR%"
+:_configure_done
 
 
 rem *** perform GR build ***
-:gr_build
+:_build
 echo.
 echo GAMMARAY-BUILD %_GR_VERSION% (%_GR_BUILD_TYPE%)
 pushd "%_GR_BUILD_DIR%"
 call cmake --build . --parallel 4 --config %_GR_BUILD_TYPE%
 popd
-:gr_build_done
+:_build_done
 
 rem *** perform QT install ***
-:gr_install
-:gr_install_do
-  echo.
-  echo GAMMARAY-INSTALL %_GR_VERSION% (%_GR_BUILD_TYPE%)
-  pushd "%_GR_BUILD_DIR%"
-  call cmake --install .
-  popd
-:gr_install_test
+:_install
+rem todo: skip install when already done...
+:_install_do
+echo.
+echo GAMMARAY-INSTALL %_GR_VERSION% (%_GR_BUILD_TYPE%)
+pushd "%_GR_BUILD_DIR%"
+call cmake --install .
+popd
 
-rem todo...
-goto :gr_install_done
+:_install_test
+rem todo: validate install success...
+goto :_install_done
 rem call which Qt6WebSockets.dll 1>nul 2>nul
-rem if %ERRORLEVEL% NEQ 0 set "PATH=%PATH%;%_QT_BIN_DIR%\bin"
+rem if %ERRORLEVEL% NEQ 0 set "PATH=%PATH%;%QT_BIN_DIR%\bin"
 rem call which Qt6WebSockets.dll 1>nul 2>nul
-rem if %ERRORLEVEL% EQU 0 echo QT-INSTALL %_QT_VERSION% available &goto :gr_install_done
+rem if %ERRORLEVEL% EQU 0 echo QT-INSTALL %_QT_VERSION% available &goto :_install_done
 rem echo error: QT-INSTALL %_QT_VERSION% failed
-goto :Exit
+goto :_exit
 
-:gr_install_done
+:_install_done
 rem -- create shortcuts
 echo @start /D "%_GR_BIN_DIR%\bin" /MAX /B %_GR_BIN_DIR%\bin\gammaray.exe %%*>"%MAKER_BIN%\gammaray.bat"
 call "%MAKER_BUILD%\validate_gammaray.bat"
 
 
-:Exit
+:_exit
 cd /d "%_GR_DIR%"
 cd /d "%_BGR_START_DIR%"
 set _BGR_START_DIR=
-set _REBUILD=
+call "%MAKER_SCRIPTS%\clear_temp_envs.bat" "_GR_"
+rem set _GR_REBUILD=
 rem set _GR_VERSION=
 rem set _GR_DIR=
 rem set _GR_SOURCES_DIR=
