f3b3bc3dc294a9c40dad6bcfdded357904673b3f
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-02-03 14:19:32 +0100

    fixed clone_3d and updated clone scripts

--- scripts/clone_3d.bat
+++ scripts/clone_3d.bat
@@ -2,6 +2,10 @@
 @echo off
 call "%~dp0\maker_env.bat"
 
-set "3DPRINT_DIR=%MAKER_ROOT%\3D-Print"
+rem we cannot use "%3DPRNT_DIR%" because: https://stackoverflow.com/questions/30109655/valid-batch-file-variable-names
+set "_3DPRINT_DIR=%MAKER_ROOT%\3D-Print"
 
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%3DPRINT_DIR%" "https://github.com/hankst69/3D-Print.git" --changeDir
+call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_3DPRINT_DIR%" "https://github.com/hankst69/3D-Print.git" --changeDir
+
+rem set "3DPRINT_DIR=%_3DPRINT_DIR%"
+rem set _3DPRINT_DIR=
--- scripts/core/script_caller.bat
+++ scripts/core/script_caller.bat
@@ -1,38 +1,40 @@
 @echo off
-set "CLASS_NAME=%~1"
-set "BUILD_SCRIPTS=%~dp0.."
+set "_SCRIPTS_DIR=%~dp0.."
+set "_CLASS_NAME=%~1"
 
 if "%~2" equ "" (
   SETLOCAL ENABLEEXTENSIONS
   SETLOCAL ENABLEDELAYEDEXPANSION
-  echo USAGE:  %CLASS_NAME% ^<project_name^> [version]
+  echo USAGE:  %_CLASS_NAME% ^<project_name^> [version]
   echo available projects:
-  for /f "tokens=*" %%b in ('dir /b "%BUILD_SCRIPTS%\%CLASS_NAME%_*.bat"') do (
-    set "_BUILD_SCRIPT=%%~nb"
-    set "_BUILD_SCRIPT=!_BUILD_SCRIPT:%CLASS_NAME%_=!"
-    echo  %CLASS_NAME% !_BUILD_SCRIPT!
-    set _BUILD_SCRIPT=
+  for /f "tokens=*" %%b in ('dir /b "%_SCRIPTS_DIR%\%_CLASS_NAME%_*.bat"') do (
+    set "_PROJECT_SCRIPT=%%~nb"
+    set "_PROJECT_NAME=!_PROJECT_SCRIPT:%_CLASS_NAME%_=!"
+    echo  %_CLASS_NAME% !_PROJECT_NAME!
+    set _PROJECT_SCRIPT=
+    set _PROJECT_NAME=
   )
   goto :Exit
 )
 
 shift
-set "PROJ_NAME=%~1"
-set PROJ_ARGS=
+set "_PROJECT_NAME=%~1"
+set  _PROJECT_ARGS=
 :param_loop
 shift
-if "%~1" neq "" set "PROJ_ARGS=%PROJ_ARGS% %1"
+if "%~1" neq "" set "_PROJECT_ARGS=%_PROJECT_ARGS% %1"
 if "%~1" neq "" goto :param_loop
 
-if not exist "%BUILD_SCRIPTS%\%CLASS_NAME%_%PROJ_NAME%.bat" (
-  echo error: unknown project '%PROJ_NAME%' ^("%CLASS_NAME%_%PROJ_NAME%.bat" does not exist^)
+if not exist "%_SCRIPTS_DIR%\%_CLASS_NAME%_%_PROJECT_NAME%.bat" (
+  echo error: unknown project '%_PROJECT_NAME%' ^("%_CLASS_NAME%_%_PROJECT_NAME%.bat" does not exist^)
   goto :Exit
 )
 
-call "%BUILD_SCRIPTS%\%CLASS_NAME%_%PROJ_NAME%.bat" %PROJ_ARGS%
+echo call "%_SCRIPTS_DIR%\%_CLASS_NAME%_%_PROJECT_NAME%.bat" %_PROJECT_ARGS%
+call "%_SCRIPTS_DIR%\%_CLASS_NAME%_%_PROJECT_NAME%.bat" %_PROJECT_ARGS%
 
 :Exit
-set BUILD_SCRIPTS=
-set CLASS_NAME=
-set PROJ_NAME=
-set PROJ_ARGS=
+set _SCRIPTS_DIR=
+set _CLASS_NAME=
+set _PROJECT_NAME=
+set _PROJECT_ARGS=
