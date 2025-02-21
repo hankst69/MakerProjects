2c116b4824e6ae923243a37e8b99f7f1871b1a3c
hankst69 <steffen_hanke@web.de>
2025-02-21 23:12:04 +0100

    update clone scripts: added SingleFile user script and AppleTV3 JailBreak

--- scripts/clone_UserScripts.bat
+++ scripts/clone_UserScripts.bat
@@ -1,5 +1,6 @@
 @rem https://github.com/hankst69/UserScript-KnowHow
 @rem https://github.com/hankst69/UserScripts
+@rem https://github.com/gildas-lormeau/SingleFile-MV3
 @echo off
 call "%~dp0\maker_env.bat"
 
@@ -8,3 +9,4 @@ set "_USERSCRIPTS_KNOWHOW_DIR=%MAKER_PROJECTS_WEB%\KnowHow"
 
 @call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_USERSCRIPTS_KNOWHOW_DIR%" "https://github.com/hankst69/UserScript-KnowHow"
 @call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_USERSCRIPTS_DIR%" "https://github.com/hankst69/UserScripts" --changeDir --switchBranch DLWSMEDIA_with_HLSPlayer
+@call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_USERSCRIPTS_DIR%\SingleFile-MV3" "https://github.com/gildas-lormeau/SingleFile-MV3"
