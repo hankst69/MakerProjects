c0246ba5d64e6e1fc77dad3292722fe5a1cfd86c
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-09-03 09:04:41 +0200

    updated Arduino ESP32 support

---
+++ ArduinoSketches/BoardAndModuleInfos/config_ArduinoIDE.bat
@@ -0,0 +1,13 @@
+@echo off
+echo.
+echo For ESP8266 and RP2040 sboard support add these entries to the Arduino IDE -^> Preferences -^> Additional boards manager URLs:
+echo.
+echo https://arduino.esp8266.com/stable/package_esp8266com_index.json
+echo https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
+echo.
+echo. 
+echo For installing ESP32 BLE Mouse/Keyboard/Gamepad libraries call:
+echo.
+echo clone ArduinoESPLibraries  (%~dp0scrips\clone_ArduinoESPLibraries)
+
+
