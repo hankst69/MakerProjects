418da2278bee99d9181c305747910f62459cf96d
hankst69 <steffen_hanke@web.de>
2025-09-08 17:39:28 +0200

    updated ESP32C3 CW-Keyer project before switch to 'nimBLE' bluetooth stack

--- ArduinoSketches/ESP/ESP32C3-SuperMiniOled/AbrobotESP32C3SMO_CW-BT-Keyer/AbrobotESP32C3SMO_CW-BT-Keyer.ino
+++ ArduinoSketches/ESP/ESP32C3-SuperMiniOled/AbrobotESP32C3SMO_CW-BT-Keyer/AbrobotESP32C3SMO_CW-BT-Keyer.ino
@@ -3,6 +3,12 @@
 // https://github.com/nrfconnect/sdk-zephyr/tree/main/boards/shields/abrobot_esp32c3_oled
 // Abrobot esp32c3 oled only works with sh1106_compatible display driver. It does not support 1306 display driver commands. Its screen resolution is 72x40. Its screen start position is 30, 12.
 
+//#include <BLEDevice.h>
+//#include <BLEUtils.h>
+//#include <BLEScan.h>
+//#include <BLEAdvertisedDevice.h>
+
+//#include "Arduino.h"
 #include "BleKeyboard.h"
 //#include "BleAbsMouse.h"
 #include "U8g2lib.h"
@@ -13,7 +19,7 @@
 #define ESPC3MINIOLED_OLED_PIN_CLOCK 6
 #define ESPC3MINIOLED_OLED_PIN_DATA  5
 
-BleKeyboard bleKeyboard;
+BleKeyboard bleKeyboard("CW-BT-Keyer");
 //BleAbsMouse bleAbsMouse;
 ESPC3MINIOLED_OLED_U8G2TYPE u8g2(ESPC3MINIOLED_OLED_ROTATION, ESPC3MINIOLED_OLED_PIN_RESET, ESPC3MINIOLED_OLED_PIN_CLOCK, ESPC3MINIOLED_OLED_PIN_DATA);
 
@@ -22,18 +28,17 @@ void setup() {
   bleKeyboard.begin();	
   //bleAbsMouse.begin();
   u8g2.begin();
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_6x10_tf);
-  u8g2.drawStr(0, 0, "CW-BT-Keyer");
-  u8g2.sendBuffer();
 }
 
-
 void loop() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.drawStr(0, 10, "CW-BT-Keyer");
+
   if(bleKeyboard.isConnected()) {
-    u8g2.drawStr(0, 10, "BT-Kbd connected");
+    u8g2.drawStr(0, 30, "BT-Kbd connected");
 	} else {
-    u8g2.drawStr(0, 10, "BT-Kbd ...");
+    u8g2.drawStr(0, 30, "BT-Kbd ...");
 	}
   // if(bleAbsMouse.isConnected()) {
   //   u8g2.drawStr(0, 20, "BT-Mouse connected");
@@ -41,6 +46,7 @@ void loop() {
   //   u8g2.drawStr(0, 10, "BT-Mouse ...");
 	// }
 
+  u8g2.sendBuffer();
   if(!bleKeyboard.isConnected() /*&& !bleAbsMouse.isConnected()*/) {
     delay(1000);
 		return;
