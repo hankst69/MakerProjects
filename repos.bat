acb0f809b2fcb8246d73d5117a5d2b4999bdf6c9
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-09-16 16:00:27 +0200

    updated git repository finder script

--- repos.bat
+++ repos.bat
@@ -1,4 +1,6 @@
 @echo off
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
 call :List_Git_Repos_in_Dir "%~dp0"
 goto :EOF
 
@@ -8,6 +10,11 @@ goto :EOF
 
 :Dump_Git_Repo
 pushd "%~1"
-for /f "tokens=2,3" %%i in ('call git remote -v') do @if /I "%%j" equ "(push)" echo. "%~1" 		 ^(%%i^)
+>"%TEMP%\strlen.tmp" echo."%~1"
+set _STRING_LENGTH=0
+for %%? in (%TEMP%\strlen.tmp) do (set /A _STRING_LENGTH=%%~z? - 4)
+set "_RIGHT_PADDING= "
+for /L %%i in (!_STRING_LENGTH!,1,60) do set "_RIGHT_PADDING=!_RIGHT_PADDING! "
+for /f "tokens=2,3" %%i in ('call git remote -v') do @if /I "%%j" equ "(push)" echo. "%~1"!_RIGHT_PADDING!   	^(%%i^)
 popd
 goto :EOF
