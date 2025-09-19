b2ce5cede3b474aad87713752de52d2c4ec13eae
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-09-19 16:25:40 +0200

    added support for hanDev repository

--- scripts/clone_Python.bat
+++ scripts/clone_Python.bat
@@ -2,6 +2,7 @@
 @echo off
 call "%~dp0\maker_env.bat"
 
+set "MAKER_PROJECTS_PYTHON=%MAKER_PROJECTS%\Python"
 set "_PYTHON_PROJECTS_DIR=%MAKER_PROJECTS_PYTHON%"
 
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_PYTHON_PROJECTS_DIR%" "https://github.com/hankst69/Python.git" --changeDir
