0b49f4790ecd7774319fd363cef2a97e3ec3103a
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-10-11 00:43:00 +0200

    updated clone scripts for renamed repositories

--- scripts/clone_Solar2024.bat
+++
@@ -1,7 +0,0 @@
-@rem @call "%~dp0scripts\clone_in_folder.bat" "%~dp0projects\Solar2024" "https://github.com/hankst69/Solardach_2024.git" --changeDir
-@echo off
-call "%~dp0\maker_env.bat"
-
-set "_SOLAR24_DIR=%MAKER_PROJECTS%\Solar2024"
-
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_SOLAR24_DIR%" "https://github.com/hankst69/Solardach_2024.git" --changeDir
--- scripts/clone_hanDev.bat
+++
@@ -1,8 +0,0 @@
-@rem https://github.com/hankst69/han_Dev.git
-@echo off
-call "%~dp0\maker_env.bat"
-
-set "MAKER_PROJECTS_HANDEV=%MAKER_PROJECTS%\hanDev"
-set "_HANDEV_PROJECTS_DIR=%MAKER_PROJECTS_HANDEV%"
-
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_HANDEV_PROJECTS_DIR%" "https://github.com/hankst69/hanDev.git" --changeDir
