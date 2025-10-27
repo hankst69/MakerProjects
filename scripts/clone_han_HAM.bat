7a88a911361167fa21aeab77d5a7951427961a52
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-10-27 23:02:16 +0100

    updated clone_han_HAM script

--- scripts/clone_han_HAM.bat
+++ scripts/clone_han_HAM.bat
@@ -4,5 +4,9 @@ call "%~dp0\maker_env.bat"
 
 set "MAKER_PROJECTS_HANHAM=%MAKER_PROJECTS%\han_HAM"
 set "_HAN_HAM_DIR=%MAKER_PROJECTS_HANHAM%"
+set "_HAN_HAM_LIC_DIR=%MAKER_PROJECTS_HANHAM%\Amateurfunk-Lizenz"
 
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_HAN_HAM_DIR%" "https://github.com/hankst69/han_HAM.git" --changeDir
+call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_HAN_HAM_DIR%" "https://github.com/hankst69/han_HAM.git"
+call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_HAN_HAM_LIC_DIR%" "https://github.com/hankst69/han_HAM_license.git"
+
+cd /d "%_HAN_HAM_DIR%"
