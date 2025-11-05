07b8f8687d98c95ceb12e4b550673c4ebc2967ca
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-11-05 17:15:53 +0100

    fixes and improvements in maker scripts to support wfview building

---
+++ scripts/core/set_version_env.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if "%~1" equ "" if "%MAKER_ENV_NOERROS%" equ "" echo error: missing arg1 'env_variable_base_name' &goto :EOF
+set %~1_VERSION=
+set %~1_VERSION_MAJOR=
+set %~1_VERSION_MINOR=
+set %~1_VERSION_PATCH=
+
+if "%~2" equ "" goto :EOF
+call "%MAKER_SCRIPTS%\split_version.bat" "%~2" 1>nul
+set "%~1_VERSION=%VERSION%"
+set "%~1_VERSION_MAJOR=%VERSION_MAJOR%"
+set "%~1_VERSION_MINOR=%VERSION_MINOR%"
+set "%~1_VERSION_PATCH=%VERSION_PATCH%"
