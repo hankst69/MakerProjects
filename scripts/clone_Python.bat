d7085a5a4c61b9f1f632aa61561bdc3b9f24a2d1
hankst69 <steffen_hanke@web.de>
2026-01-07 13:54:46 +0100

    updated clone Python

--- scripts/clone_Python.bat
+++ scripts/clone_Python.bat
@@ -6,10 +6,10 @@ set "MAKER_PROJECTS_PYTHON=%MAKER_PROJECTS%\Python"
 
 set "_PYTHON_PROJECTS_DIR=%MAKER_PROJECTS_PYTHON%"
 set "_PYTHON_ITKNOTEBOOKS_DIR=%MAKER_PROJECTS_PYTHON%\Jupyter\SimpleITK-Notebooks"
-set "_PYTHON_GITFILTERREPO_DIR=%MAKER_PROJECTS_PYTHON%\Git-Filter-Repo"
-
 
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_PYTHON_PROJECTS_DIR%"      "https://github.com/hankst69/Python.git" --changeDir
 call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_PYTHON_ITKNOTEBOOKS_DIR%"  "https://github.com/InsightSoftwareConsortium/SimpleITK-Notebooks.git"
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_PYTHON_GITFILTERREPO_DIR%" "https://github.com/newren/git-filter-repo"
+
+rem set "_PYTHON_GITFILTERREPO_DIR=%MAKER_PROJECTS_PYTHON%\Git-Filter-Repo"
+rem call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_PYTHON_GITFILTERREPO_DIR%" "https://github.com/newren/git-filter-repo"
 rem python "%_PYTHON_GITFILTERREPO_DIR%\git-filter-repo" --help
