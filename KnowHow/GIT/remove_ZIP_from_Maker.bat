59dd39ad82d9b18f18b5f87d89fff72425b4f4e5
hankst69 <steffen_hanke@web.de>
2026-01-25 23:52:37 +0100

    updated clone scripts and GIT KnowHow (move of ArduinoSketches into own repository)

---
+++ KnowHow/GIT/remove_ZIP_from_Maker.bat
@@ -0,0 +1,2 @@
+call git_rm_permanent "https://github.com/hankst69/MakerProjects" "ArduinoSketches\BoardAndModuleInfos\Arduino_3.5inch_LCD_Shield\3.5inch_Arduino_8BIT_Module_ILI9486_MAR3501_V1.1.zip"
+rem call git_rm_permanent "https://github.com/hankst69/MakerProjects" "ArduinoSketches\BoardAndModuleInfos\Arduino_3.5inch_LCD_Shield\3.5inch_Arduino_8BIT_Module_ILI9486_MAR3501_V1.1.zip" --test
--- scripts/clone_ArduinoSketches.bat
+++ scripts/clone_ArduinoSketches.bat
@@ -1,30 +1,12 @@
 @echo off
 call "%~dp0\maker_env.bat"
 
+@rem https://github.com/hankst69/ArduinoSketches
 set "_ARDUINO_SKETCHES_DIR=%MAKER_PROJECTS%\ArduinoSketches"
+call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_ARDUINO_SKETCHES_DIR%" "https://github.com/hankst69/ArduinoSketches.git"
 
-@rem https://github.com/hankst69/espBode
-@rem https://github.com/Hamhackin/espBode
-@rem https://github.com/timkoers/espBode
-@rem https://github.com/sq6sfo/espBode
-set "_AS_espBode_DIR=%_ARDUINO_SKETCHES_DIR%\AWGControl\espBode"
-set "_AS_espBode-awg_DIR=%_ARDUINO_SKETCHES_DIR%\AWGControl\espBode-awg"
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_AS_espBode_DIR%" "https://github.com/hankst69/espBode.git"
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_AS_espBode-awg_DIR%" "https://github.com/hankst69/espBode.git" --switchBranch awgDevices
-
-@rem "https://github.com/hankst69/fygen"
-@rem "https://github.com/mattwach/fygen"
-rem  set "_AS_fygen_DIR=%_ARDUINO_SKETCHES_DIR%\AWGControl\fygen"
-rem call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_AS_fygen_DIR%" "https://github.com/hankst69/fygen.git" --changeDir
-
-@rem https://github.com/hankst69/SimpleCWKeyer.git
-rem set "_AS_SimpleCWKeyer_DIR=%_ARDUINO_SKETCHES_DIR%\unoR3\SimpleCWKeyer"
-set "_AS_SimpleCWKeyer_DIR=%_ARDUINO_SKETCHES_DIR%\SimpleCWKeyer"
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_AS_SimpleCWKeyer_DIR%" "https://github.com/hankst69/SimpleCWKeyer.git"
-
-@rem https://github.com/hankst69/ats-mini.git
-set "_AS_ATS-Mini_DIR=%_ARDUINO_SKETCHES_DIR%\ATS-Mini"
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_AS_ATS-Mini_DIR%" "https://github.com/hankst69/ats-mini.git"
-
+call "%_ARDUINO_SKETCHES_DIR%\clone_projects.bat"
+call "%_ARDUINO_SKETCHES_DIR%\clone_libraries.bat"
 
 cd /d "%_ARDUINO_SKETCHES_DIR%"
+goto :EOF
