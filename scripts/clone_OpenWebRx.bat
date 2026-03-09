5980ac70158301d6b53ab04fb2246461401c48fa
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2026-03-09 21:14:37 +0100

    updated clone OpenWebRx

--- scripts/clone_OpenWebRx.bat
+++ scripts/clone_OpenWebRx.bat
@@ -1,9 +1,20 @@
+@rem "https://github.com/ha7ilm/openwebrx"
 @rem "https://github.com/jketterl/openwebrx.git"
+@rem "https://github.com/luarvique/openwebrx.git"
 @echo off
 call "%~dp0\maker_env.bat"
 
+rem OpenWebRX  https://www.openwebrx.de/
+rem OpenWebRX+ https://fms.komkon.org/OWRX/
+
 set "MAKER_PROJECTS_OPENWEBRX=%MAKER_PROJECTS%\OpenWebRx"
 set "_OPENWEBRX_DIR=%MAKER_PROJECTS_OPENWEBRX%"
+set "_OPENWEBRX_SRC_DIR=%MAKER_PROJECTS_OPENWEBRX%\openwebrx"
+set "_OPENWEBRX+_SRC_DIR=%MAKER_PROJECTS_OPENWEBRX%\openwebrx+"
 
-call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_OPENWEBRX_DIR%" "https://github.com/jketterl/openwebrx.git" --changeDir --switchBranch develop
-rem call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_OPENWEBRX_DIR%" "https://github.com/jketterl/openwebrx.git" --changeDir --switchBranch sstv
+
+call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_OPENWEBRX_SRC_DIR%"  "https://github.com/jketterl/openwebrx.git" --switchBranch develop
+call "%MAKER_SCRIPTS%\clone_in_folder.bat" "%_OPENWEBRX+_SRC_DIR%" "https://github.com/luarvique/openwebrx.git"
+
+cd /d "%_OPENWEBRX_DIR%"
+dir /ad
