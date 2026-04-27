7d82d54881af5db38aee70a2ad8137a21b5071b0
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2026-04-27 23:01:36 +0200

    added clone Kodi script

---
+++ scripts/clone_Kodi.bat
@@ -0,0 +1,26 @@
+@rem Kodi
+@rem https://kodi.wiki/view/Main_Page
+@rem https://kodi.wiki/view/Official_add-on_repository
+@echo off
+call "%~dp0\maker_env.bat"
+
+@rem https://github.com/xbmc/xbmc.git
+@rem https://github.com/xbmc/repo-plugins.git
+@rem https://github.com/xbmc/repo-scripts.git
+
+rem https://kodi.wiki/view/Add-on:Surveillance_Cameras
+rem https://github.com/b-jesch/plugin.video.ipcams/tree/Matrix
+
+set "_KODI_DIR=%MAKER_DIR_PROJECTS%\Kodi"
+
+set "_KODI_XBMC_DIR=%_KODI_DIR%\xbmc"
+set "_KODI_PLUGINS_DIR=%_KODI_DIR%\plugins"
+set "_KODI_SCRIPTS_DIR=%_KODI_DIR%\scripts"
+set "_KODI_PLUGINS_IPCAMS_DIR=%_KODI_DIR%\plugin_ipcams"
+
+rem call "%MAKER_ENV_CORE%\clone_in_folder.bat" "%_KODI_XBMC_DIR%" "https://github.com/xbmc/xbmc.git" %* --switchBranch Helix
+call "%MAKER_ENV_CORE%\clone_in_folder.bat" "%_KODI_PLUGINS_DIR%" "https://github.com/xbmc/repo-plugins.git" %* --switchBranch helix
+call "%MAKER_ENV_CORE%\clone_in_folder.bat" "%_KODI_SCRIPTS_DIR%" "https://github.com/xbmc/repo-scripts.git" %* --switchBranch helix
+call "%MAKER_ENV_CORE%\clone_in_folder.bat" "%_KODI_PLUGINS_IPCAMS_DIR%" "https://github.com/b-jesch/plugin.video.ipcams.git" --switchBranch Matrix
+
+cd /d "%_KODI_DIR%"
